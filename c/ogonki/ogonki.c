c44044a96d662cbd806eb14f5a622daa09a5fef5
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2009-02-26 12:38:23 +0000

    Program ogonki akceptuje wiele plików podanych jako parametry

--- c/ogonki/ogonki.c
+++ c/ogonki/ogonki.c
@@ -1,6 +1,6 @@
 /*
-Zamienia polskie znaki z ogonkami na litery lacinskie w danym pliku
-Sposob uzycia: ogonki [plik]
+Zamienia polskie znaki z ogonkami na litery lacinskie w podanych plikach
+Uzycie: ogonki [pliki...]
  */
 
 #include <stdio.h>
@@ -10,56 +10,58 @@ Sposob uzycia: ogonki [plik]
 
 int main (int argc, char *argv[])
 {
-  int i;
+  int i, j;
   char c;
   char tekst[BUFSIZE];
   FILE *f;
 
 /* sprawdzenie, czy podano plik do przepisania */
   if (argc < 2){
-    printf("Program zamienia polskie znaki z ogonkami na litery lacinskie\n");
-    printf("Skladnia: ogonki [plik]\n");
+    printf("Zamienia polskie znaki z ogonkami na litery lacinskie\n");
+    printf("Uzycie: ogonki [pliki...]\n");
   }
   else
   {
-  /* wczytanie zawartosci podanego pliku do tablicy tekst[] */
-    f=fopen(argv[1], "r+");
-    if (f == NULL)
-      fprintf(stderr, "blad: nie mozna otworzyc pliku %s\n", argv[1]);
-    else /* jesli plik istnieje, wykonuj program */
-    {
-      for (i=0; (c=fgetc(f)) != EOF; i++)
-	tekst[i]=c;
-      tekst[i]=EOF;
+    for (j=1; j < argc; j++) {
+    /* wczytanie zawartosci podanego pliku do tablicy tekst[] */
+      f=fopen(argv[j], "r+");
+      if (f == NULL)
+	fprintf(stderr, "blad: nie mozna otworzyc pliku %s\n", argv[1]);
+      else /* jesli plik istnieje, wykonuj program */
+      {
+	for (i=0; (c=fgetc(f)) != EOF; i++)
+	  tekst[i]=c;
+	tekst[i]=EOF;
 
-      for (i=0; tekst[i] != EOF; i++)
-	switch(tekst[i]){
-	  case '¹': tekst[i]='a'; break;
-	  case 'æ': tekst[i]='c'; break;
-	  case 'ê': tekst[i]='e'; break;
-	  case '³': tekst[i]='l'; break;
-	  case 'ñ': tekst[i]='n'; break;
-	  case 'ó': tekst[i]='o'; break;
-	  case 'œ': tekst[i]='s'; break;
-	  case '¿': tekst[i]='z'; break;
-	  case 'Ÿ': tekst[i]='z'; break;
+	for (i=0; tekst[i] != EOF; i++)
+	  switch(tekst[i]){
+	    case '¹': tekst[i]='a'; break;
+	    case 'æ': tekst[i]='c'; break;
+	    case 'ê': tekst[i]='e'; break;
+	    case '³': tekst[i]='l'; break;
+	    case 'ñ': tekst[i]='n'; break;
+	    case 'ó': tekst[i]='o'; break;
+	    case 'œ': tekst[i]='s'; break;
+	    case '¿': tekst[i]='z'; break;
+	    case 'Ÿ': tekst[i]='z'; break;
 
-	  case '¥': tekst[i]='A'; break;
-	  case 'Æ': tekst[i]='C'; break;
-	  case 'Ê': tekst[i]='E'; break;
-	  case '£': tekst[i]='L'; break;
-	  case 'Ñ': tekst[i]='N'; break;
-	  case 'Ó': tekst[i]='O'; break;
-	  case 'Œ': tekst[i]='S'; break;
-	  case '¯': tekst[i]='Z'; break;
-	  case '': tekst[i]='Z'; break;
-	}
+	    case '¥': tekst[i]='A'; break;
+	    case 'Æ': tekst[i]='C'; break;
+	    case 'Ê': tekst[i]='E'; break;
+	    case '£': tekst[i]='L'; break;
+	    case 'Ñ': tekst[i]='N'; break;
+	    case 'Ó': tekst[i]='O'; break;
+	    case 'Œ': tekst[i]='S'; break;
+	    case '¯': tekst[i]='Z'; break;
+	    case '': tekst[i]='Z'; break;
+	  }
 
-      rewind(f);
-      for (i=0; tekst[i] != EOF; i++)
-	fputc(tekst[i], f);
+	rewind(f);
+	for (i=0; tekst[i] != EOF; i++)
+	  fputc(tekst[i], f);
 
-      fclose(f);
+	fclose(f);
+      }
     }
   }
   return 0;
