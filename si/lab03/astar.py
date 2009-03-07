18008e79c7b635e5f1ceefc95e2832af644614bc
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2009-03-07 00:46:45 +0000

    Sztuczna inteligencja: astar.py

--- si/lab03/astar.py
+++ si/lab03/astar.py
@@ -19,19 +19,25 @@ class Node:
 	def __init__(self, name):
 		self.name = name
 		self.neighbors = {} # klucz: węzeł sąsiadujący, wartość: koszt
+		self.previous = None
+
+	def __cmp__(self, node):
+		return cmp(self.f, node.f)
+
+	def __eq__(self, node):
+		return self.name == node.name
+
+	def __hash__(self):
+		return hash(self.name)
 
 	def __str__(self):
 		description = self.name + ' ->'
 		for neighbor, cost in self.neighbors.iteritems():
-			description += ' ' + neighbor.name + ' ' + cost
+			description += ' ' + neighbor.name + ' ' + str(cost)
 		description += ', h=' + str(self.h)
+		if self.previous:
+			description += ', prev=' + self.previous.name
 		return description
-
-	def __cmp__(self, node):
-		return cmp(self.name, node.name)
-	
-	def __hash__(self):
-		return hash(self.name)
 
 
 def read_input():
@@ -40,7 +46,6 @@ def read_input():
 	graph_filename = sys.argv[1]
 	h_filename = sys.argv[2]
 	w0_name = sys.argv[3]
-
 	# tworzymy węzły
 	graph_file = open(graph_filename)
 	for line in graph_file.readlines():
@@ -57,7 +62,7 @@ def read_input():
 		node_name = line_data[0]
 		neighbor_name = line_data[1]
 		neighbor = graph[neighbor_name]
-		cost = line_data[2].strip()
+		cost = float(line_data[2].strip())
 		graph[node_name].neighbors[neighbor] = cost
 	graph_file.close()
 	# dodajemy informacje o heurezie do węzłów
@@ -70,7 +75,45 @@ def read_input():
 		if h == 0:
 			wg = graph[node_name]
 	h_file.close()
+	if w0_name not in graph:
+		print 'Podanego węzła początkowego nie ma w grafie.'
+		sys.exit(1)
 	w0 = graph[w0_name]
+
+def a_star(w0, wg):
+	'Algorytm A*.'
+	open_nodes.append(w0)
+	w0.g = .0
+	w0.f = w0.h
+	while len(open_nodes) != 0:
+		node = min(open_nodes)
+		if node == wg:
+			return path(wg)
+		open_nodes.remove(node)
+		closed_nodes.append(node)
+		for neighbor in node.neighbors:
+			if neighbor in closed_nodes:
+				continue
+			current_g = node.g + node.neighbors[neighbor]
+			is_current_better = False
+			if neighbor not in open_nodes:
+				open_nodes.append(neighbor)
+				is_current_better = True
+			elif current_g < neighbor.g:
+				is_current_better = True
+			if is_current_better:
+				neighbor.previous = node
+				neighbor.g = current_g
+				neighbor.f = neighbor.g + neighbor.h
+	return None
+
+def path(node):
+	'Zwraca ścieżkę powstałą w wyniku działania algorytmu A*.'
+	path = []
+	while node.previous:
+		path.append(node)
+		node = node.previous
+	return reversed(path)
 
 
 if __name__ == '__main__':
@@ -80,9 +123,21 @@ if __name__ == '__main__':
 			print 'Graf wejściowy:'
 			for node in graph.values():
 				print node
-			print 'Idziemy z', w0.name, 'do', wg.name
+
+			path = a_star(w0, wg)
+
+			if path is not None:
+				print '\nNajkrótsza ścieżka z', w0.name, 'do', wg.name + ':'
+				path_length = .0
+				print w0.name, path_length,
+				for node in path:
+					path_length += node.previous.neighbors[node]
+					print '->', node.name, path_length,
+			else:
+				print '\nNie znaleziono ścieżki.'
 		else:
 			print usage
 	except IOError:
 		print 'Nie można odnaleźć podanego pliku.'
+		sys.exit(1)
 
