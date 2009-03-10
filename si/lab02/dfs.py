79bdd16c25574a2ba1e997b9029c1c6208dec0d6
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2009-03-10 16:59:37 +0000

    SI: dfs.py: jeszcze nie działa dobrze (zrobić zbiory zamiast list)

---
+++ si/lab02/dfs.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import re
+import sys
+
+
+usage = u"""\
+Algorytm Depth First Search, znajdujący najkrótszą ścieżkę pomiędzy danymi
+wierzchołkami grafu.
+Użycie: python dfs.py PLIK_GRAFU\
+"""
+
+graph = {} # klucz: id węzła, wartość: węzeł
+open_nodes = [] # węzły do rozwinięcia
+closed_nodes = [] # zbadane węzły
+path = []
+w0 = None # węzeł początkowy
+wg = None # węzeł końcowy
+
+
+class Node:
+    def __init__(self, id):
+        self.id = id
+        self.neighbors = {} # klucz: id sąsiada, wartość: koszt
+        self.previous = None
+
+    def cost(self, neighbor):
+        u"""Zwróć koszt przejścia z tego węzła do podanego sąsiada."""
+        return self.neighbors[neighbor]
+
+    def __cmp__(self, node):
+        u"""Funkcja kosztu."""
+        return cmp(self.f, node.f)
+
+    def __eq__(self, node):
+        return self.id == node.id
+
+    def __hash__(self):
+        return self.id
+
+    def __str__(self):
+        description = str(self.id) + ' ->'
+        for neighbor, cost in self.neighbors.iteritems():
+            description += ' %d(%d)' % (neighbor, cost)
+        if self.previous:
+            description += ', prev=' + self.previous.id
+        return description
+
+
+def read_input():
+    global w0
+    global wg
+    graph_filename = sys.argv[1]
+    graph_file = open(graph_filename)
+    lines = graph_file.readlines()
+    # dodajemy węzły
+    for node_id, line in enumerate(lines):
+        if line == '\n':
+            break
+        line_data = re.split(' +', line)
+        node = Node(node_id)
+        # dodajemy sąsiadów węzła i ich koszty
+        for j, cost in enumerate(line_data):
+            cost = int(cost)
+            if cost >= 0:
+                node.neighbors[j] = cost
+        graph[node_id] = node
+    last_line = re.split(' +', lines[-1])
+    graph_file.close()
+    w0 = graph[int(last_line[0])]
+    wg = graph[int(last_line[1])]
+
+
+def dfs(wg):
+    u"""Algorytm Depth First Search szukania najkrótszej ścieżki."""
+    if not open_nodes:
+        return False
+    node = min(open_nodes)
+    open_nodes.remove(node)
+    closed_nodes.append(node)
+    path.append(node)
+    print 'ENTER DFS:'
+    printlist('OPEN:', open_nodes)
+    printlist('CLOSED:', closed_nodes)
+    printlist('PATH:', path)
+    print 'z', node.id ,'do wyboru:', path[-1].neighbors
+    for neighbor_id in path[-1].neighbors:
+        neighbor = graph[neighbor_id]
+        if neighbor not in closed_nodes:
+            neighbor.f = path[-1].f + node.cost(neighbor.id)
+            open_nodes.append(neighbor)
+            neighbor.previous = node
+    if path[-1] == wg:
+        return True
+    else:
+        if dfs(wg):
+            return True
+        else:
+            path.pop()
+            return dfs(wg)
+
+def printlist(name, list):
+    print name,
+    for node in list:
+        print str(node.id) + ',',
+    print
+
+
+if __name__ == '__main__':
+    try:
+        if len(sys.argv) > 1:
+            read_input()
+            print u'Graf wejściowy:'
+            for node in graph.values():
+                print node
+
+            w0.f = 0
+            open_nodes.append(w0)
+            print u'\nNajkrótsza ścieżka z %d do %d:' % (w0.id, wg.id)
+            if dfs(wg):
+                path_length = .0
+                #print w0.id, path_length,
+                for node in path:
+                    #path_length += node.previous.cost(node)
+                    print '->', node.id,# path_length,
+            else:
+                print u'Nie znaleziono ścieżki.'
+        else:
+            print usage
+    except IOError:
+        print u'Nie można odnaleźć podanego pliku.'
+        sys.exit(1)
+
