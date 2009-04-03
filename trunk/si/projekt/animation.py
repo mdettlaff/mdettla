499c0e2069d8910b73db4b358d51a3d19abf6cd3
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2009-04-03 16:42:09 +0000

    SI: projekt: animation.py: timer

--- trunk/si/projekt/animation.py
+++ trunk/si/projekt/animation.py
@@ -18,7 +18,15 @@ import ga_keyb
 usage = u"""Użycie: python animation.py PLIK_TEKSTOWY..."""
 
 WIDTH = 516
-HEIGHT = 192
+HEIGHT = 196
+iterations = ga_keyb.DEFAULT_ITERATIONS
+population_size = ga_keyb.DEFAULT_POPULATION_SIZE
+tournament_size = ga_keyb.DEFAULT_TOURNAMENT_SIZE
+p_c = ga_keyb.DEFAULT_P_C # prawdopodobieństwo krzyżowania
+p_m = ga_keyb.DEFAULT_P_M # prawdopodobieństwo mutacji
+weights = ga_keyb.DEFAULT_WEIGHTS
+words = ga_keyb.DEFAULT_WORDS
+encoding = ga_keyb.DEFAULT_ENCODING
 
 
 class AnimationWindow(QtGui.QMainWindow):
@@ -28,10 +36,7 @@ class AnimationWindow(QtGui.QMainWindow):
         self.setWindowTitle('Algorytm genetyczny - animacja')
         self.canvas = KeyboardCanvas(self) # obszar rysowania
         self.setCentralWidget(self.canvas)
-        self.statusbar = self.statusBar()
-        self.connect(self.canvas,
-                QtCore.SIGNAL('messageToStatusbar(QString)'), self.statusbar,
-                QtCore.SLOT('showMessage(QString)'))
+        self.canvas.start()
         self.center()
 
     def center(self):
@@ -45,8 +50,14 @@ class KeyboardCanvas(QtGui.QFrame):
     u"""Obszar na którym będziemy rysować klawiaturę."""
     def __init__(self, parent):
         QtGui.QFrame.__init__(self, parent)
+        self.timer = QtCore.QBasicTimer()
         self.setFocusPolicy(QtCore.Qt.StrongFocus)
+        self.keyboard_image = QtGui.QImage('keyboard.png')
         self.current_layout = None
+        self.generator = genetic_algorithm()
+
+    def start(self):
+        self.timer.start(0, self)
 
     def paintEvent(self, event):
         painter = QtGui.QPainter(self)
@@ -55,53 +66,42 @@ class KeyboardCanvas(QtGui.QFrame):
 
         painter.fillRect(0, 0, WIDTH, HEIGHT, white) # tło
         painter.setPen(black)
-        painter.drawImage(4, 4, QtGui.QImage('keyboard.png'))
-        left_margins = [62, 75, 95]
-        top_margins = [53, 86, 120]
+        painter.drawImage(4, 4, self.keyboard_image)
         if self.current_layout:
+            left_margins = [62, 75, 95]
+            top_margins = [53, 86, 120]
             for i, row in enumerate(self.current_layout.phenotype):
                 x = left_margins[i]
                 y = top_margins[i]
                 for key in row:
                     painter.drawText(x, y, key.upper())
                     x += 34
+            painter.drawText(5, 186, 'Pokolenie ' + str(self.generation_count)
+                    + ', przystosowanie ' + str(self.current_layout.fitness))
 
-    def nextGeneration(self, generation_count, keyboard_layout):
-        self.current_layout = keyboard_layout
-        self.update()
-        self.emit(QtCore.SIGNAL('messageToStatusbar(QString)'),
-                'Pokolenie ' + str(generation_count) +
-                ', przystosowanie ' + str(self.current_layout.fitness))
+    def timerEvent(self, event):
+        try:
+            self.generation_count, self.current_layout = self.generator.next()
+            self.repaint()
+        except StopIteration:
+            self.timer.stop()
 
 
-class GeneticAlgorithmThread(threading.Thread):
-    def run(self):
-        iterations = ga_keyb.DEFAULT_ITERATIONS
-        population_size = ga_keyb.DEFAULT_POPULATION_SIZE
-        tournament_size = ga_keyb.DEFAULT_TOURNAMENT_SIZE
-        p_c = ga_keyb.DEFAULT_P_C # prawdopodobieństwo krzyżowania
-        p_m = ga_keyb.DEFAULT_P_M # prawdopodobieństwo mutacji
-        weights = ga_keyb.DEFAULT_WEIGHTS
-        words = ga_keyb.DEFAULT_WORDS
-        encoding = ga_keyb.DEFAULT_ENCODING
-        corpus = ga_keyb.Corpus(sys.argv[1:], encoding, words)
-        #results = []
-        for i, best in enumerate(ga_keyb.epoch(iterations, population_size,
-            p_c, p_m, ga_keyb.select_tournament, [tournament_size],
-            ga_keyb.fitness, [corpus] + weights)):
-            #results.append(best)
-            animation.canvas.nextGeneration(i+1, best)
-            print '%d\t%d' % (i+1, best.fitness)
+def genetic_algorithm():
+    for i, best in enumerate(ga_keyb.epoch(iterations, population_size,
+        p_c, p_m, ga_keyb.select_tournament, [tournament_size],
+        ga_keyb.fitness, [corpus] + weights)):
+        yield (i+1, best)
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
+        print u'Analiza statystyczna tekstu...'
+        corpus = ga_keyb.Corpus(sys.argv[1:], encoding, words)
+
         app = QtGui.QApplication(sys.argv)
         animation = AnimationWindow()
         animation.show()
-
-        GeneticAlgorithmThread().start()
-
         sys.exit(app.exec_())
     else:
         print __doc__[:-1]
