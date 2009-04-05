95fdbd506ecf9442100b4787aab83cc23e191438
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2009-04-05 20:03:01 +0000

    Python: sudoku.py, utworzenie katalogu scripts

--- trunk/python/sudoku.py
+++ trunk/python/sudoku.py
@@ -5,15 +5,19 @@ u"""Program rozwiązujący Sudoku algorytmem Depth-First Search z heurystyką.""
 
 __author__ = u'Michał Dettlaff'
 
+import math
+import random
 import sys
 
 
 usage = u"""Użycie: python sudoku.py"""
 
-N = 4
+N = 9
+UNIT_SIZE = int(math.sqrt(N)) # rozmiar dużego kwadratu
+
 open_nodes = [] # tuple: (heurystyka węzła, węzeł do rozwinięcia)
 closed_nodes = [] # zbadane węzły
-solution = [] # kolejne liczby dodawane na planszę wraz z pozycjami
+solution = [] # kolejne cyfry dodawane na planszę
 calls_count = 0 # liczba wywołań funkcji dfs()
 
 
@@ -21,88 +25,69 @@ def print_solution(solution):
     board = []
     for i in range(N):
         board.append(N*[0])
-    for position in solution:
-        board[position[0].y][position[0].x] = position[1]
+    for i, num in enumerate(solution):
+        x = i % N
+        y = i / N
+        board[y][x] = num
     s = ''
     for y, row in enumerate(board):
         for x, square in enumerate(row):
             s += str(square)
-            #if x % 3 == 2:
-            #    s += ' '
-        #if y % 3 == 2:
-        #    s += '\n'
+            if x % UNIT_SIZE == 2:
+                s += ' '
+        if y % UNIT_SIZE == 2:
+            s += '\n'
         s += '\n'
-    print s[:-2]
+    print s[:-1]
 
-def is_consistent(board, value, coords):
-    if board[coords.y][coords.x] != 0:
+def is_consistent(board, value, solution):
+    if len(solution) == N*N:
+        return True
+    value_x = len(solution) % N
+    value_y = len(solution) / N
+    if board[value_y][value_x] != 0:
         return False
     # rząd
     for x in range(N):
-        if board[coords.y][x] == value:
+        if board[value_y][x] == value:
             return False
     # kolumna
     for y in range(N):
-        if board[y][coords.x] == value:
+        if board[y][value_x] == value:
             return False
     # kwadrat
-    #sq = lambda z: 3 * (z / 3)
-    #for y in range(sq(coords.y), 3 + sq(coords.y)):
-    #    for x in range(sq(coords.x), 3 + sq(coords.x)):
-    #        if board[y][x] == value:
-    #            return False
+    sq = lambda z: UNIT_SIZE * (z / UNIT_SIZE)
+    for y in range(sq(value_y), UNIT_SIZE + sq(value_y)):
+        for x in range(sq(value_x), UNIT_SIZE + sq(value_x)):
+            if board[y][x] == value:
+                return False
     return True
 
 def is_completed(solution):
-    board = []
-    for i in range(N):
-        board.append(N*[0])
-    for position in solution:
-        #print position
-        board[position[0].y][position[0].x] = position[1]
-    for row in board:
-        if 0 in row:
-            return False
-    return True
+    return len(solution) == N*N
 
 def neighbors(solution):
     board = []
     for i in range(N):
         board.append(N*[0])
-    for position in solution:
-        #print position
-        board[position[0].y][position[0].x] = position[1]
+    for i, num in enumerate(solution):
+        x = i % N
+        y = i / N
+        board[y][x] = num
     nbors = []
-    for y in range(N):
-        for x in range(N):
-            for a in range(1, N+1):
-                if is_consistent(board, a, Coords(x, y)):
-                    neighbor = solution + [(Coords(x, y), a)]
-                    nbors.append(neighbor)
+    for a in range(1, N+1):
+        if is_consistent(board, a, solution):
+            neighbor = solution + [a]
+            nbors.append(neighbor)
     return nbors
-
-
-class Coords:
-    u"""Przechowuje współrzędne (x, y)."""
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    #def __eq__(self, other):
-    #    return self.x == other.x and self.y == other.y
-
-    def __ne__(self, other):
-        return self.x != other.x or self.y != other.y
-
-    def __str__(self):
-        return '(' + str(self.x) + ', ' + str(self.y) + ')'
 
 
 def sudoku_solution_dfs(w0):
     open_nodes.append((0, [w0]))
-    print dfs(N)
-    print solution
-    print_solution(solution)
+    if dfs(N):
+        return solution
+    else:
+        return None
 
 
 def dfs(N):
@@ -110,9 +95,6 @@ def dfs(N):
     global solution
     global calls_count
     calls_count += 1
-    print len(open_nodes), len(solution)
-    print_solution(solution)
-    print
     if not open_nodes:
         return False
     #h_node = min(open_nodes) # tupla: (heurystyka, węzeł o min. heurystyce)
@@ -124,13 +106,14 @@ def dfs(N):
     for neighbor in neighbors(solution):
         if neighbor not in closed_nodes:
             nbors.append(neighbor)
+        else:
+            'closed!'
     open_nodes.extend(heuristics(nbors))
     if is_completed(solution):
         return True
     elif dfs(N):
         return True
     elif solution:
-        print 'pop'
         solution.pop()
         return dfs(N)
     else:
@@ -139,16 +122,15 @@ def dfs(N):
 
 def heuristics(solutions):
     u"""Funkcja oceny (heurystyka): oceń podane rozwiązania."""
+    # TODO
     solutions_with_heuristics = []
     for s in solutions:
-        import random
-        solutions_with_heuristics.append((random.randint(0, 100), s))
+        solutions_with_heuristics.append((0, s))
     return solutions_with_heuristics
 
 
 if __name__ == '__main__':
-    sys.setrecursionlimit(20000)
-    sudoku_solution_dfs((Coords(2, 2), 1))
-    #print Solution()
-    print u'Liczba wywołań:', calls_count
+    sudoku_solution_dfs(random.randint(1, N))
+    print_solution(solution)
+    print u'Liczba wywołań Depth-First Search:', calls_count
 
