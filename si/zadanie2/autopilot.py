2a1d00625daf566fb22c360d4bd35b025e15f22e
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2009-04-09 16:24:33 +0000

    SI: zadanie2: animacja lądowania samolotu

--- si/zadanie2/autopilot.py
+++ si/zadanie2/autopilot.py
@@ -10,14 +10,12 @@ import sys
 
 
 INFINITY = 65535
-h_integ = .1
-u"""Krok przy liczeniu całki (im mniejszy, tym lepsza dokładność)."""
 
 
 class FuzzySet:
     u"""Zbiór rozmyty."""
     def degree(self, x):
-        u"""Stopień przynależności wartości `x` do tego zbioru.
+        u"""Zwróć stopień przynależności wartości `x` do tego zbioru.
 
         :Return:
             Stopień przynależności. Przyjmuje wartości od 0 do 1 włącznie.
@@ -41,37 +39,35 @@ class TriangleSet(FuzzySet):
     def degree(self, x):
         u"""Trójkątna funkcja przynależności."""
         x = float(x)
-        deg = 0
+        degree = 0
         if x <= self.a:
-            deg = 0
+            degree = 0
         elif x <= self.b:
             if self.a != -INFINITY:
-                deg = (x - self.a) / (self.b - self.a)
+                degree = (x - self.a) / (self.b - self.a)
             else:
-                deg = 1
+                degree = 1
         elif x <= self.c:
             if self.c != INFINITY:
-                deg = (self.c - x) / (self.c - self.b)
+                degree = (self.c - x) / (self.c - self.b)
             else:
-                deg = 1
+                degree = 1
         else:
-            deg = 0
+            degree = 0
         if hasattr(self, 'multiplier') and self.multiplier is not None:
-            return deg * self.multiplier(x)
+            return degree * self.multiplier(x)
         else:
-            return deg
+            return degree
 
 
-class LimitedSet(TriangleSet):
-    def __init__(self, triangle_set, limit):
-        u"""Zbiór trójkątny ograniczony z góry przez prostą y=`limit`."""
-        self.a = triangle_set.a
-        self.b = triangle_set.b
-        self.c = triangle_set.c
+class LimitedSet(FuzzySet):
+    u"""Zbiór trójkątny ograniczony z góry przez prostą y=`limit`."""
+    def __init__(self, set, limit):
+        self.base_set = set
         self.limit = limit
 
     def degree(self, x):
-        return min(TriangleSet.degree(self, x), self.limit)
+        return min(self.base_set.degree(x), self.limit)
 
 
 class MultiSet(FuzzySet):
@@ -83,14 +79,65 @@ class MultiSet(FuzzySet):
         return max([set.degree(x) for set in self.sets])
 
 
-def integral(set, func, a, b, h):
+def integral(f, a, b, h):
     u"""Całkowanie numeryczne metodą trapezów."""
-    integ = 0
+    integral = 0
     x = a
     while x < b:
-        integ += h/2 * (func(set, x) + func(set, x + h))
+        integral += h/2 * (f(x) + f(x + h))
         x += h
-    return integ
+    return integral
+
+
+def fuzzy_controller(x1_sets, x2_sets, FAM, x1_init, x2_init, x1_next, x2_next,
+        iterations, u_min, u_max, h):
+    u"""Sterownik rozmyty.
+
+    :Parameters:
+        - `FAM`: Rozmyta pamięć asocjacyjna.
+        - `iterations`: Liczba iteracji algorytmu.
+        - `u_min`: Minimum dziedziny zbioru rozmytego.
+        - `u_max`: Maksimum dziedziny zbioru rozmytego.
+        - `h`: Krok przy liczeniu całki (im mniejszy, tym lepsza dokładność).
+
+    :Return:
+        Zwracaj za pomocą generatora:
+            - Wartość x1 w danej iteracji.
+            - Wartość x2 w danej iteracji.
+            - Wartość sterowania u w danej iteracji.
+
+    """
+    x1 = x1_init
+    x2 = x2_init
+    t = 0
+    for iter in range(iterations):
+        conclusions = {} # klucz: zbiór, wartość: stopień przynależności
+        for j, x1_set in enumerate(x1_sets):
+            for i, x2_set in enumerate(x2_sets):
+                deg_x2 = x2_set.degree(x2) # stopień przynależności do zbioru
+                deg_x1 = x1_set.degree(x1) # stopień przynależności do zbioru
+                if deg_x2 and deg_x1:
+                    if FAM[j][i] not in conclusions:
+                        # agregacja
+                        conclusions[FAM[j][i]] = min(deg_x2, deg_x1)
+                    else:
+                        conclusions[FAM[j][i]] = \
+                            max(conclusions[FAM[j][i]], min(deg_x2, deg_x1))
+        # aktywacja i akumulacja
+        # wynikowy zbiór rozmyty
+        sum_conclusion = MultiSet([LimitedSet(c, limit) for \
+                c, limit in conclusions.iteritems()])
+        # wyostrzanie metodą środka ciężkości
+        i1 = integral(lambda x: sum_conclusion.degree(x) * x, \
+                u_min, u_max, h)
+        i2 = integral(sum_conclusion.degree, u_min, u_max, h)
+        u = i1/i2
+
+        yield (x1, x2, u)
+        x1_prev = x1
+        x1 = x1_next(x1, x2)
+        x2 = x2_next(x1_prev, x2, u)
+        t += 1
 
 
 def main(argv):
@@ -102,85 +149,38 @@ def main(argv):
         else:
             return 0
 
-    U_MIN = -20
-    u'Minimum dziedziny zbioru rozmytego.'
-    U_MAX = 20
-    u'Maksimum dziedziny zbioru rozmytego.'
-
-    # h
     NZ = TriangleSet(0, 0, 500)
     S = TriangleSet(-200, 300, 800, positive)
     M = TriangleSet(300, 800, 1300, lambda x: 1 - positive(x - 1000))
     L = TriangleSet(500, 1000, 1000)
 
-    # v, f
     DL = TriangleSet(-INFINITY, -20, -10)
     DS = TriangleSet(-20, -10, 0)
     Z = TriangleSet(-10, 0, 10)
     US = TriangleSet(0, 10, 20)
     UL = TriangleSet(10, 20, INFINITY)
 
-    v_sets = [DL, DS, Z, US, UL]
     h_sets = [L, M, S, NZ]
+    v_sets = [DL, DS, Z, US, UL]
 
     FAM = [ [ Z, DS, DL, DL, DL],
             [US,  Z, DS, DL, DL],
             [UL, US,  Z, DS, DL],
             [UL, UL,  Z, DS, DS] ]
 
-    t = 0
-    h = 1000
-    v = -20
+    h_next = lambda h, v: h + v
+    v_next = lambda h, v, f: v + f
 
-    for wibble in range(200):
-        conclusions = {} # {zbiór: stopień przynależności}
-        for j, h_set in enumerate(h_sets):
-            for i, v_set in enumerate(v_sets):
-                deg_v = v_set.degree(v) # stopień przynależności do zbioru
-                deg_h = h_set.degree(h) # stopień przynależności do zbioru
-                if deg_v and deg_h:
-                    if FAM[j][i] not in conclusions:
-                        conclusions[FAM[j][i]] = min(deg_v, deg_h)
-                    else:
-                        conclusions[FAM[j][i]] = \
-                                max(conclusions[FAM[j][i]], min(deg_v, deg_h))
-        # wynikowy zbiór rozmyty
-        sum_conclusion = MultiSet([LimitedSet(c, limit) for \
-            c, limit in conclusions.iteritems()])
-        # wyostrzanie
-        i1 = integral(sum_conclusion, \
-                lambda self, x: MultiSet.degree(self, x) * x, \
-                U_MIN, U_MAX, h_integ)
-        i2 = integral(sum_conclusion, MultiSet.degree, \
-                U_MIN, U_MAX, h_integ)
-        center_of_gravity = i1/i2
+    h0 = 1000
+    v0 = -20
 
-        f = center_of_gravity
-        print '%.5f\t%.5f\t%.5f' % (h, v, f)
-        h = h + v
-        v = v + f
-        t += 1
-
-    # debug
-    """
-    for conclusion, deg in conclusions.iteritems():
-        print conclusion, deg
-    for i in range(-15, 25):
-        print 'w(' + str(i) + ')=' + str(sum_conclusion.degree(i)) + ','
-    print
-    print 'f = %.2f/%.2f = %.2f' % (i1, i2, center_of_gravity)
-    print integral(Z, lambda self, x: TriangleSet.degree(self, x) * x, \
-            0, INFINITY-1, h_integ)
-    """
+    for h, v, f in fuzzy_controller(h_sets, v_sets, FAM, h0, v0,
+            h_next, v_next, 200, -20, 20, .1):
+        print '%8.3f\t%8.4f\t%8.5f' % (h, v, f)
 
 
     """
-    # wahadło
-
-    U_MIN = -24
-    u'Minimum dziedziny zbioru rozmytego.'
-    U_MAX = 24
-    u'Maksimum dziedziny zbioru rozmytego.'
+    # odwrócone wahadło
 
     NEG1 = TriangleSet(-INFINITY, -2, 0)
     Z1 = TriangleSet(-2, 0, 2)
@@ -201,54 +201,17 @@ def main(argv):
             [ N, Z,  P],
             [ Z, P, PB] ]
 
-    t = 0
-    x1 = 1
-    x2 = -4
+    x1_next = lambda x1, x2: x1 + x2
+    x2_next = lambda x1, x2, u: x1 + x2 - u
 
-    for wibble in range(4):
-        conclusions = {} # {zbiór: stopień przynależności}
-        for j, x1_set in enumerate(x1_sets):
-            for i, x2_set in enumerate(x2_sets):
-                deg_x2 = x2_set.degree(x2) # stopień przynależności do zbioru
-                deg_x1 = x1_set.degree(x1) # stopień przynależności do zbioru
-                if deg_x2 and deg_x1:
-                    if FAM[j][i] not in conclusions:
-                        conclusions[FAM[j][i]] = min(deg_x2, deg_x1)
-                    else:
-                        conclusions[FAM[j][i]] = \
-                           max(conclusions[FAM[j][i]], min(deg_x2, deg_x1))
-        # wynikowy zbiór rozmyty
-        sum_conclusion = MultiSet([LimitedSet(c, limit) for \
-            c, limit in conclusions.iteritems()])
-        # wyostrzanie
-        i1 = integral(sum_conclusion, \
-                lambda self, x: MultiSet.degree(self, x) * x, \
-                U_MIN, U_MAX, h_integ)
-        i2 = integral(sum_conclusion, MultiSet.degree, \
-                U_MIN, U_MAX, h_integ)
-        center_of_gravity = i1/i2
+    x1_init = 1
+    x2_init = -4
 
-        u = center_of_gravity
-        print '%8.5f\t%8.5f\t%8.5f' % (x1, x2, u)
-        x1_prev = float(x1)
-        x1 = x1 + x2
-        x2 = x1_prev + x2 - u
-        t += 1
+    for x1, x2, u in fuzzy_controller(x1_sets, x2_sets, FAM, x1_init, x2_init,
+            x1_next, x2_next, 4, -24, 24, .1):
+        print '%8.3f\t%8.4f\t%8.5f' % (x1, x2, u)
     """
 
-    # debug
-    """
-    for conclusion, deg in conclusions.iteritems():
-        print conclusion, deg
-    x = -20
-    while x < 20:
-        print 'w(' + str(x) + ')=' + str(sum_conclusion.degree(x))
-        x += .5
-    print
-    print 'u = %.2f/%.2f = %.2f' % (i1, i2, center_of_gravity)
-    #print integral(Z, lambda self, x: TriangleSet.degree(self, x) * x, \
-    #        0, INFINITY-1, h_integ)
-    """
 
 if __name__ == '__main__':
     main(sys.argv)
