c489ec048c4c207081e8875642d02ff6103947bf
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2009-04-21 17:11:44 +0000

    Python: delnlateof.py: ulepszenia

--- trunk/python/scripts/delnlateof.py
+++ trunk/python/scripts/delnlateof.py
@@ -3,15 +3,28 @@
 
 u"""Usuń znak nowej linii na końcu podanego pliku."""
 
+import os
 import sys
 
-usage = u'Użycie: ' + sys.argv[0] + u' PLIK'
+usage = u'Użycie: ' + sys.argv[0] + u' PLIK...'
 
-if len(sys.argv) == 2:
-    file = open(sys.argv[1], 'r+')
-    data = file.read()
-    if data.endswith('\n'):
-        file.truncate(len(data) - 1)
-else:
-    print usage
+def main(argv):
+    if len(argv) > 1:
+        for filename in argv[1:]:
+            if os.path.isdir(filename):
+                print u'błąd:', filename, u'jest katalogiem'
+                continue
+            file = open(filename, 'rb+')
+            data = file.read()
+            if '\0' in data:
+                print u'błąd:', filename, u'jest plikiem binarnym'
+                continue
+            if data.endswith('\n') and not data.endswith('\r\n'):
+                file.truncate(len(data) - 1)
+            file.close()
+    else:
+        print usage
+
+if __name__ == '__main__':
+    main(sys.argv)
 
