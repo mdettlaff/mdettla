adb6717d8c59ff295c8c5e4a4883e59203824bf4
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2009-05-31 12:28:45 +0000

    SI: bayes.py: klasyfikator drzew

--- si/lab08/bayes.py
+++ si/lab08/bayes.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-u"""Naiwny klasyfikator bayesowski."""
+u"""Przykład zastosowania naiwnego klasyfikatora bayesowskiego."""
+
+__docformat__ = 'restructuredtext pl'
+__author__ = u'Michał Dettlaff'
 
 
 class NBC:
@@ -11,7 +14,7 @@ class NBC:
         self.database = []
 
     def add_classifiable(self, classifiable):
-        u"""Dodaj dany obiekt do bazy danych.
+        u"""Dodaj obiekt do bazy danych.
 
         Jeśli podany obiekt nie ma z góry określonej klasy do której powinien
         należeć, zostaje przed dodaniem zaklasyfikowany przez klasyfikator.
@@ -21,75 +24,108 @@ class NBC:
 
         """
         if classifiable.domain is None:
-            classifiable.domain = self.classify(self, classifiable)
+            classifiable.domain = self.classify(classifiable)
         self.database.append(classifiable)
 
     def classify(self, classifiable):
         u"""Zwróć kategorię do której zaklasyfikowany zostanie dany obiekt."""
         # klasa -> prawdopodobieństwo przynależności obiektu do tej klasy
         probabilities = {}
-        all_domains = set()
-        for cl in self.database:
-            all_domains.add(cl.domain)
+        all_domains = set([cl.domain for cl in self.database])
         for domain in all_domains:
-            # tworzymy listę obiektów należących do danej klasy
-            classifiables_in_domain = []
-            for cl in self.database:
-                if domain == cl.domain:
-                    classifiables_in_domain.append(cl)
+            classifiables_in_domain = \
+                    [cl for cl in self.database if domain == cl.domain]
             # prawdopodobieństwo że obiekt należy do klasy; najpierw bierzemy
             # prawdopodobieństwo a priori
-            probability = float(len(classifiables_in_domain)) \
-                    / len(self.database)
+            probability = \
+                    float(len(classifiables_in_domain)) / len(self.database)
             for feature in classifiable.features:
-                # obliczamy ilość obiektów tej klasy mających daną cechę
-                classifiables_having_feature_count = 0
-                for cl in classifiables_in_domain:
-                    if classifiable.features[feature] == cl.features[feature]:
-                        classifiables_having_feature_count += 1
-                # obliczamy ilość możliwych wartości cechy
-                possible_feature_values = set()
-                for cl in self.database:
-                    possible_feature_values.add(cl.features[feature])
-                # obliczamy zgodnie ze wzorem na prawdopodobieństwo dla
-                # danej cechy
-                probability *= (1.0 + classifiables_having_feature_count) \
+                classifiables_having_feature = \
+                        [cl for cl in classifiables_in_domain if \
+                        classifiable.features[feature] == cl.features[feature]]
+                possible_feature_values = \
+                        set([cl.features[feature] for cl in self.database])
+                # obliczamy prawdopodobieństwo dla danej cechy, stosując
+                # wygładzanie Laplace'a
+                probability *= (1.0 + len(classifiables_having_feature)) \
                         / (len(classifiables_in_domain) + \
                         len(possible_feature_values))
             probabilities[domain] = probability
         print probabilities # DEBUG
         return max(probabilities, key = lambda x: probabilities[x])
 
+    def __str__(self):
+        return '\n'.join([unicode(cl) for cl in self.database])
+
 
 class Classifiable:
     u"""Obiekt, który może zostać poddany klasyfikacji przez klasyfikator."""
-    def __init__(self, domain = None, **kwargs):
+    def __init__(self, id, domain, **features):
+        u"""Utwórz obiekt do klasyfikacji lub dodania do bazy klasyfikatora.
+
+        :Parameters:
+            - `id`: Unikatowy identyfikator obiektu.
+            - `domain`: Klasa, do której należy obiekt. Jeśli klasa jest równa
+              `None`, obiekt zostanie przydzielony do odpowiedniej klasy przez
+              klasyfikator.
+            - `features`: Słownik z cechami obiektu i ich wartościami.
+
+        """
+        self.id = id
         self.domain = domain
-        self.features = kwargs
+        self.features = features
+
+    def __str__(self):
+        s = ''
+        s += '%s\t%s\t' % (self.id, self.domain)
+        for feature, feature_value in self.features.iteritems():
+            s += feature + ': ' + feature_value + ', '
+        return s[:-2]
 
 
-class Dog(Classifiable):
-    # długość ogona
-    VERY_SHORT = 0
-    SHORT = 1
-    MEDIUM = 2
-    LONG = 3
-    VERY_LONG = 4
-    # kolor nosa
-    BLACK = 0
-    PINK = 1
+class Tree(Classifiable):
+    # długości
+    KROTKIE = u'krótkie'
+    SREDNIE = u'średnie'
+    DLUGIE = u'długie'
+    BARDZO_DLUGIE = u'b. długie'
+
+    def __init__(self, id, domain, dlugosc_szpilek_mm, dlugosc_szyszek_mm):
+        self.id = id
+        self.domain = domain
+        self.features = {}
+        # podział na kategorie długości szpilek
+        if dlugosc_szpilek_mm < 20:
+            self.features[u'długość szpilek'] = Tree.KROTKIE
+        elif dlugosc_szpilek_mm < 30:
+            self.features[u'długość szpilek'] = Tree.SREDNIE
+        elif dlugosc_szpilek_mm < 40:
+            self.features[u'długość szpilek'] = Tree.DLUGIE
+        else:
+            self.features[u'długość szpilek'] = Tree.BARDZO_DLUGIE
+        # podział na kategorie długości szyszek
+        if dlugosc_szyszek_mm < 50:
+            self.features[u'długość szyszek'] = Tree.KROTKIE
+        elif dlugosc_szyszek_mm < 100:
+            self.features[u'długość szyszek'] = Tree.SREDNIE
+        else:
+            self.features[u'długość szyszek'] = Tree.DLUGIE
 
 
 def main():
     classifier = NBC()
 
-    classifier.add_classifiable(Dog('spaniel',
-        tail_len = Dog.MEDIUM, nose_color = Dog.BLACK))
-    classifier.add_classifiable(Dog('golden retriever',
-        tail_len = Dog.LONG, nose_color = Dog.BLACK))
+    classifier.add_classifiable(Tree(1, u'sosna', 33, 40))
+    classifier.add_classifiable(Tree(2, u'jodła', 27, 13))
+    classifier.add_classifiable(Tree(3, u'sosna', 42, 55))
+    classifier.add_classifiable(Tree(4, u'świerk', 11, 120))
+    print u'Baza danych o drzewach:\n', unicode(classifier)
 
-    print classifier.classify(Dog(
-        tail_len = Dog.MEDIUM, nose_color = Dog.PINK))
+    print u'Klasyfikujemy nowe drzewo:'
+    new_tree = Tree(5, None, 22, 130)
+    print unicode(new_tree)
+    classifier.add_classifiable(new_tree)
+    print unicode(new_tree)
 
 
 if __name__ == '__main__':
