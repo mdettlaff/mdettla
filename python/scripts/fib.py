36f793d07bd0288c24034086a8b4ab295ac040d2
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2009-11-09 13:59:48 +0000

    Python: porządki

--- python/scripts/fib.py
+++ python/scripts/fib.py
@@ -1,16 +1,10 @@
 import sys
 
-def fib(x):
-    if x == 0:
-        return 0
-    elif x == 1:
-        return 1
-    else:
-        return fib(x-2) + fib(x-1)
+fib = lambda x: fib(x-2) + fib(x-1) if x > 1 else x
 
 if len(sys.argv) == 2:
-    n = sys.argv[1]
+    n = int(sys.argv[1])
 else:
     n = 10
-for i in range(0, int(n)):
+for i in range(0, n):
     print fib(i),
