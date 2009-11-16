036a33de514d20796b732f469b59e754b769eddb
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2009-11-16 13:18:51 +0000

    Algorytmy ewolucyjne: zadanie 1

--- ea/zadanie1/make.sh
+++ ea/zadanie1/make.sh
@@ -3,5 +3,10 @@ then
   javac -d bin -sourcepath src -cp ../JGA/bin:lib/commons-math-2.0.jar \
   -Xlint:unchecked src/*.java
 else
-  java -cp ../JGA/bin:lib/commons-math-2.0.jar:bin GAMaxIndependentSet
+  if [ "$1" -eq "1" ]
+  then
+    java -cp bin:../JGA/bin:lib/commons-math-2.0.jar GAMaxIndependentSet
+  else
+    java -cp bin:../JGA/bin MaxIndependentSet
+  fi
 fi
