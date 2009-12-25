b61fd96aefc94c24b389998293562fa1317ea32c
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2009-12-25 15:21:10 +0000

    ImageProcessing: uaktualnienie make.sh

--- sem/ImageProcessing/make.sh
+++ sem/ImageProcessing/make.sh
@@ -5,18 +5,19 @@ then
     mkdir bin
   fi
   javac -d bin src/mdettla/imgproc/*.java \
-  src/mdettla/imgproc/fuzzy/edge/*.java \
-  src/mdettla/imgproc/edge/*.java
+  src/mdettla/imgproc/algorithms/fuzzy/edge/*.java \
+  src/mdettla/imgproc/algorithms/edge/*.java
 else
   if [ $1 == "edge" ]
   then
     java -cp bin mdettla.imgproc.ProcessImage \
-    mdettla.imgproc.edge.SobelAlgorithm $2 $3
+    mdettla.imgproc.algorithms.edge.SobelAlgorithm $2 $3
   else
     if [ $1 == "fuzzyedge" ]
     then
       java -cp bin mdettla.imgproc.ProcessImage \
-      mdettla.imgproc.fuzzy.edge.EdgeDetectionWithFuzzyClassifier $2 $3
+      mdettla.imgproc.algorithms.fuzzy.edge.EdgeDetectionWithFuzzyClassifier \
+      $2 $3
     else
       echo "Nieznany parametr"
     fi
