4161c779db66b7ca01b4972169caa7b03c18bb84
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2010-01-01 18:13:40 +0000

    SystemyAgentowe: tłumaczenie z agnielskiego na niemiecki

--- agent/projekt/src/pl/edu/agh/jadex/TranslateOnline.java
+++ agent/projekt/src/pl/edu/agh/jadex/TranslateOnline.java
@@ -1,50 +1,62 @@
 package pl.edu.agh.jadex;
 
 import jadex.runtime.Plan;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SuppressWarnings("serial")
 public class TranslateOnline extends Plan {
 
-	public TranslateOnline() {
+	protected static Map<String, String> dictionary;
+
+	public TranslateOnline() {}
+
+	public static Map<String, String> getDictionary() {
+		if (dictionary == null) {
+			dictionary = new HashMap<String, String>();
+			dictionary.put("coffee", "Kaffee");
+			dictionary.put("milk", "Milch");
+			dictionary.put("cow", "Kuh");
+			dictionary.put("dog", "Hund");
+			dictionary.put("cat", "Katze {f} [zool.] | Katzen {pl}");
+		}
+		return dictionary;
 	}
 
 	public void body() {
-		String eword = (String) getParameter("word").getValue();
+		String eword = (String) getParameter("eword").getValue();
 		eword = eword.split(" ")[1];
-		URL dict = null;
-		try {
-			dict = new URL("http://wolfram.schneider.org/dict/dict.cgi?query="
-					+ eword);
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		}
-		BufferedReader in = null;
-		try {
-			in = new BufferedReader(new InputStreamReader(dict.openStream()));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		String inline = null;
-		try {
-			while ((inline = in.readLine()) != null) {
-				if (inline.indexOf("<td><b>" + eword + "</b> ") != -1) {
-					try {
-						String englishWord = inline.replaceAll(".*right\">", "");
-						englishWord = englishWord.trim();
-						getLogger().info(eword + " -> " + englishWord);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
-			}
-			in.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+//		URL dict = null;
+//		try {
+//			dict = new URL("http://wolfram.schneider.org/dict/dict.cgi?wholewords=1&query="
+//					+ eword);
+//		} catch (MalformedURLException e) {
+//			e.printStackTrace();
+//		}
+//		BufferedReader in = null;
+//		try {
+//			in = new BufferedReader(new InputStreamReader(dict.openStream()));
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		String inline = null;
+//		try {
+//			while ((inline = in.readLine()) != null) {
+//				if (inline.indexOf("><b>" + eword + "</b> |") != -1) {
+//					try {
+//						String gword = inline.replaceAll(".*left\"><td>", "");
+//						gword = gword.replaceAll("<td>.*", "");
+//						gword = gword.trim();
+						getLogger().info(eword + " -> " + getDictionary().get(eword));
+//					} catch (Exception e) {
+//						e.printStackTrace();
+//					}
+//				}
+//			}
+//			in.close();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
 	}
 }
