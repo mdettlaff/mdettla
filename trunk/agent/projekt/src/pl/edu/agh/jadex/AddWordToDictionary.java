63b7cc465bdc3164383acdda70e2d3f2bdc84013
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2010-01-01 19:17:36 +0000

    Systemy agentowe: projekt: beliefset

--- trunk/agent/projekt/src/pl/edu/agh/jadex/AddWordToDictionary.java
+++ trunk/agent/projekt/src/pl/edu/agh/jadex/AddWordToDictionary.java
@@ -2,7 +2,6 @@ package pl.edu.agh.jadex;
 
 import jadex.runtime.Plan;
 
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class AddWordToDictionary extends Plan {
@@ -10,22 +9,16 @@ public class AddWordToDictionary extends Plan {
 
 	public AddWordToDictionary() {}
 
-	@SuppressWarnings("unchecked")
 	public void body() {
-		Map<String, String> words = (Map<String, String>)getBeliefbase().
-		getBelief("egwords").getFact();
 		String messageContent = (String)getParameter("new_word").getValue();
 		StringTokenizer st = new StringTokenizer(messageContent);
 		if (st.countTokens() >= 3) {
 			st.nextToken();
 			String eword = st.nextToken();
-			if (!words.containsKey(eword)) {
-				String gword = st.nextToken();
-				words.put(eword, gword);
-				//getBeliefbase().getBeliefSet("egwords").addFact(new jadex.util.Tuple(key, st.nextToken()));
-				getLogger().info("dodano słowo do słownika: " +
-						eword + " " + gword);
-			}
+			String gword = st.nextToken();
+			getBeliefbase().getBeliefSet("egwords").addFact(new jadex.util.Tuple(eword, gword));
+			getLogger().info("dodano słowo do słownika: " +
+					eword + " " + gword);
 		}
 	}
 }
