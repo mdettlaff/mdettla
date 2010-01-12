94a712e3bad6a6eeaa70bb5fa4dd0614d8372b92
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2010-01-12 22:01:33 +0000

    Algorytmy ewolucyjne: wyniki do wykresów

--- ea/zadanie2/make.sh
+++ ea/zadanie2/make.sh
@@ -6,5 +6,35 @@ then
   fi
   javac -d bin src/mdettla/ea/zadanie2/*.java
 else
-  java -cp bin mdettla.ea.zadanie2.CompareAlgorithms
+  if [ ! -d output ]
+  then
+    mkdir output
+  fi
+  echo "Test 1: Funkcja Griewangka"
+  echo "Obliczam za pomocą algorytmu Differential Evolution..."
+  java -cp bin mdettla.ea.zadanie2.RunEvolutionaryAlgorithm \
+  mdettla.ea.zadanie2.DifferentialEvolutionGA 1500 \
+  mdettla.ea.zadanie2.GriewangkFunction \
+  > output/griewangk_de
+  echo "Wyniki zapisano do katalogu output"
+  echo "Obliczam za pomocą algorytmu Cumulative Step Adaptation..."
+  java -cp bin mdettla.ea.zadanie2.RunEvolutionaryAlgorithm \
+  mdettla.ea.zadanie2.CumulativeStepAdaptationES 1500 \
+  mdettla.ea.zadanie2.GriewangkFunction \
+  > output/griewangk_csa
+  echo "Wyniki zapisano do katalogu output"
+  echo
+  echo "Test 2: Funkcja Rosenbrocka"
+  echo "Obliczam za pomocą algorytmu Differential Evolution..."
+  java -cp bin mdettla.ea.zadanie2.RunEvolutionaryAlgorithm \
+  mdettla.ea.zadanie2.DifferentialEvolutionGA 5000 \
+  mdettla.ea.zadanie2.RosenbrockFunction \
+  > output/rosenbrock_de
+  echo "Wyniki zapisano do katalogu output"
+  echo "Obliczam za pomocą algorytmu Cumulative Step Adaptation..."
+  java -cp bin mdettla.ea.zadanie2.RunEvolutionaryAlgorithm \
+  mdettla.ea.zadanie2.CumulativeStepAdaptationES 5000 \
+  mdettla.ea.zadanie2.RosenbrockFunction \
+  > output/rosenbrock_csa
+  echo "Wyniki zapisano do katalogu output"
 fi
---
+++ ea/zadanie2/output/evo.plt
@@ -0,0 +1,23 @@
+set term png
+set xlabel "pokolenie"
+set ylabel "przystosowanie"
+
+set yr [0:1500]
+
+set output "griewangk_csa.png"
+set title "Strategia ewolucyjna - Cumulative Step Adaptation\nfunkcja Griewangka"
+plot "griewangk_csa" title "¶rednie przystosowanie najlepszego osobnika w populacji" with lines
+
+set output "griewangk_de.png"
+set title "Algorytm genetyczny - Differential Evolution\nfunkcja Griewangka"
+plot "griewangk_de" title "¶rednie przystosowanie najlepszego osobnika w populacji" with lines
+
+set yr [0:10000]
+
+set output "rosenbrock_csa.png"
+set title "Strategia ewolucyjna - Cumulative Step Adaptation\nfunkcja Rosenbrocka"
+plot "rosenbrock_csa" title "¶rednie przystosowanie najlepszego osobnika w populacji" with lines
+
+set output "rosenbrock_de.png"
+set title "Algorytm genetyczny - Differential Evolution\nfunkcja Rosenbrocka"
+plot "rosenbrock_de" title "¶rednie przystosowanie najlepszego osobnika w populacji" with lines
