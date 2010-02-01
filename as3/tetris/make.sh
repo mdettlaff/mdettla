e0909bbdde0b526a1ec180bdd291b55ce78483e6
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2010-02-01 09:48:35 +0000

    as3: tetris: timer i reagowanie na klawisze

---
+++ as3/tetris/make.sh
@@ -0,0 +1,51 @@
+#./make.sh
+#./make.sh run
+#./make.sh all
+
+main_class=Main
+
+function move_swf_files_to_bin {
+  base_name=${base_path_name/*\//}
+  binary_dir=${binary_file/\/$base_name.swf/}
+  if [ ! -d $binary_dir ]
+  then
+    mkdir -p $binary_dir
+  fi
+  if [ -f src/${binary_file#bin/} ]
+  then
+    mv src/${binary_file#bin/} $binary_file
+  fi
+}
+
+if [ $# == 0 ] || [ "$1" == "all" ]
+then
+  if [ ! -d bin ]
+  then
+    mkdir bin
+  fi
+  source_files=`find src -name '*.as' -or -name '*.mxml'`
+  for source_file in $source_files
+  do
+    base_path_name=${source_file#src/}
+    base_path_name=${base_path_name%.as}
+    base_path_name=${base_path_name%.mxml}
+    binary_file=bin/$base_path_name.swf
+    if [ $source_file -nt $binary_file ] || [ "$1" == "all" ]
+    then
+      mxmlc -sp=src --show-actionscript-warnings=true --strict=true \
+        --debug=true $source_file
+      if [ $? != 0 ]
+      then
+        break
+      fi
+      move_swf_files_to_bin
+    fi
+  done
+else
+  if [ "$1" == "clean" ]
+  then
+    rm -r bin
+  else
+    flashplayer bin/$main_class.swf
+  fi
+fi
