6df681532f56ac21f497f68a5a878ffe0ab5e5f3
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2010-02-03 21:21:13 +0000

    as3: tetris: web

---
+++ trunk/as3/tetris/web/template/bottom.php
@@ -0,0 +1,14 @@
+        </div>
+      </div>
+      <div id="left"> 
+ 
+<? include "menu.php" ?>
+ 
+      </div>
+      <div style="clear: both;" />
+      <div id="footer">
+<? include "footer.php" ?>
+      </div>
+    </div>
+  </body>
+</html>
---
+++ trunk/as3/tetris/web/template/head.php
@@ -0,0 +1,4 @@
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+    <meta name="Language" content="pl">
+    <link rel="stylesheet" type="text/css" href="template/style.css" media="screen" />
+    <title>Tetris</title>
