c442b7eb355ca8e4a655257d7781a25f31ddf9b7
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2010-02-14 09:26:50 +0000

    as3: tetris: web: poprawki

--- as3/tetris/web/include/log.php
+++ as3/tetris/web/include/log.php
@@ -1,17 +1,15 @@
 <?php
-
-function log_write($msg) {
-    $file = fopen('log/log.txt', 'a');
-    if (!$file) {
-        $file = fopen('../log/log.txt', 'a');
+    function log_write($msg) {
+        $file = fopen('log/log.txt', 'a');
+        if (!$file) {
+            $file = fopen('../log/log.txt', 'a');
+        }
+        if ($file) {
+            $entry = '['.date('Y/m/d h:i:s', mktime()).'] '.$msg;
+            fwrite($file, $entry."\n");
+            fclose($file);
+        } else {
+            echo 'ERROR: log file not found!';
+        }
     }
-    if ($file) {
-        $entry = '['.date('Y/m/d h:i:s', mktime()).'] '.$msg;
-        fwrite($file, $entry."\n");
-        fclose($file);
-    } else {
-        echo 'ERROR: log file not found!';
-    }
-}
-
 ?>
--- as3/tetris/web/index.php
+++ as3/tetris/web/index.php
@@ -5,9 +5,9 @@
 <h2>Tetris</h2>
 
 <?php
-if (isset($_SESSION['username'])) {
-    echo 'Witaj, '.$_SESSION['username'].'! ';
-}
+    if (isset($_SESSION['username'])) {
+        echo 'Witaj, '.$_SESSION['username']."!<br />\n";
+    }
 ?>
 Na tej stronie możesz zagrać w grę Tetris.
 
