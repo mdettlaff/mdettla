43390c4770a2035bd2bcfc906bcf6c8d6ef8845e
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2010-02-22 19:38:26 +0000

    crypto: poprawienie nazwy pliku

--- crypto/lab01/run.sh
+++ crypto/lab01/run.sh
@@ -7,4 +7,4 @@ echo "różnica między rozkładem częstotliwości występowania liter
 w zaszyfrowanym tekście a rozkładem w języku angielskim w ogóle:"
 cat $1 | ./encrypt_subst.py | ./frequencies.py frequencies_en.txt
 echo "tekst odszfrowany za pomocą analizy częstości liter:"
-cat $1 | ./encrypt_subst.py | ./decrypt_substr.py letters_by_frequency_en.txt
+cat $1 | ./encrypt_subst.py | ./decrypt_subst.py letters_by_frequency_en.txt
