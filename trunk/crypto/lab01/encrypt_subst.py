2e2e06b9ce63a87df09fa18cad055c02df15d737
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2010-02-22 13:21:16 +0000

    crypto: przykładowe teksty do zaszyfrowania

--- trunk/crypto/lab01/encrypt_subst.py
+++ trunk/crypto/lab01/encrypt_subst.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
-# -*- coding: UTF-8 -*-
 
 from string import ascii_uppercase
 import random
 import sys
 
 permutation = random.sample(ascii_uppercase, len(ascii_uppercase))
-plaintext = ''.join([line.strip() for line in sys.stdin.readlines()])
+plaintext = ''.join([line.strip().upper() for line in sys.stdin.readlines()])
 print ''.join([permutation[ascii_uppercase.index(c)] for c in plaintext])
--- trunk/crypto/lab01/run.sh
+++ trunk/crypto/lab01/run.sh
@@ -1,3 +1,8 @@
+if [ "$1" == "" ]
+then
+  echo "Użycie: ./run.sh plik_tekstowy"
+  exit
+fi
 echo "współczynnik niepodobieństwa rozkładu częstotliwości występowania liter
 w zaszyfrowanym tekście do rozkładu w języku angielskim w ogóle:"
-cat plain.txt | ./encrypt_subst.py | ./frequencies.py eng_frequencies.txt
+cat $1 | ./encrypt_subst.py | ./frequencies.py eng_frequencies.txt
