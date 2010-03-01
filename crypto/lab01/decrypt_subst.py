57e278f3dbbc294dc75b3557fb9e680cd3b6af67
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2010-03-01 14:51:27 +0000

    crypto: szyfr Vigenere'a

--- crypto/lab01/decrypt_subst.py
+++ crypto/lab01/decrypt_subst.py
@@ -3,9 +3,9 @@
 import sys
 
 std_chars_by_freq = open(sys.argv[1]).readline().strip()
-plaintext = ''.join([line.strip().upper() for line in sys.stdin.readlines()])
+encrypted = ''.join([line.strip().upper() for line in sys.stdin.readlines()])
 freqs = {}
 for c in std_chars_by_freq:
-    freqs[c] = float(plaintext.count(c)) / len(plaintext)
+    freqs[c] = float(encrypted.count(c)) / len(encrypted)
 chars_by_freq = sorted(freqs, key=freqs.__getitem__)
-print ''.join([std_chars_by_freq[chars_by_freq.index(c)] for c in plaintext])
+print ''.join([std_chars_by_freq[chars_by_freq.index(c)] for c in encrypted])
