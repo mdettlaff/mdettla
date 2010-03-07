ba265bc925bc5315f32ee4f156d9bc5f7a58660c
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2010-03-07 18:34:41 +0000

    Kod strony Szybkie Pisanie na Klawiaturze

---
+++ trunk/spnk/screenshot.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+ <META HTTP-EQUIV="Content-type" CONTENT="text/html; charset=iso-8859-2">
+ <META NAME="Language" CONTENT="pl">
+<title>Szybkie Pisanie na Klawiaturze</title>
+</head>
+
+<body bgcolor=#cacaca text=#050505 leftmargin=0 topmargin=0 marginheight=0 marginwidth=0>
+
+<?
+$fot = $_GET['fot'];
+
+if($fot)
+{
+	echo "
+	<a href=\"javascript:window.close();\">
+	<img src=\"$fot\" border=0 title=\"Zamknij okno\" /></a>
+";
+}
+?>
+
+</body>
+
+</html>
+
