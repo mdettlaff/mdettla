9bce86d1f209a73a4920d57871696b59859b6a35
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2010-03-12 18:02:47 +0000

    kspnk: programy, nauka, index

--- trunk/spnk/screenshot.php
+++ trunk/spnk/screenshot.php
@@ -1,26 +1,24 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 <html>
-<head>
- <META HTTP-EQUIV="Content-type" CONTENT="text/html; charset=iso-8859-2">
- <META NAME="Language" CONTENT="pl">
-<title>Szybkie Pisanie na Klawiaturze</title>
-</head>
+  <head>
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+    <meta name="Language" content="pl">
+    <title>Szybkie Pisanie na Klawiaturze</title>
+  </head>
 
-<body bgcolor=#cacaca text=#050505 leftmargin=0 topmargin=0 marginheight=0 marginwidth=0>
+  <body bgcolor="#CACACA" text="#050505" leftmargin="0" topmargin="0"
+      marginheight="0" marginwidth="0">
 
 <?
-$fot = $_GET['fot'];
+$pic = $_GET['pic'];
 
-if($fot)
-{
-	echo "
-	<a href=\"javascript:window.close();\">
-	<img src=\"$fot\" border=0 title=\"Zamknij okno\" /></a>
-";
+if (!empty($pic)) {
+    echo "
+<a href=\"#\" onClick=\"window.close()\">
+<img src=\"$pic\" border=\"0\" title=\"Zamknij okno\" /></a>
+    ";
 }
 ?>
 
-</body>
-
+  </body>
 </html>
-
