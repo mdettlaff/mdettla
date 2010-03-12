29b5f6dfab4a4b197c9b2d3571d2dc3ec410a9fc
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2010-03-12 18:48:14 +0000

    spnk: przystosowanie księgi do MySQL

--- spnk/sql/tables.sql
+++ spnk/sql/tables.sql
@@ -1,12 +1,9 @@
-DROP TABLE guestbook;
-DROP SEQUENCE id_guestbook_seq;
-
-CREATE SEQUENCE id_guestbook_seq;
 CREATE TABLE guestbook (
-    id_guestbook int DEFAULT NEXTVAL('id_guestbook_seq') PRIMARY KEY,
-    date_added timestamp NOT NULL,
+    id_guestbook int NOT NULL auto_increment,
+    date_added datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
     ip varchar(32),
-    username varchar NOT NULL,
-    email varchar,
-    content varchar NOT NULL
+    username varchar(32) NOT NULL,
+    email varchar(128),
+    content text NOT NULL,
+    PRIMARY KEY(id_guestbook)
 );
