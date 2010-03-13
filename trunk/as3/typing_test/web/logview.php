d746749869ff54f66f043fc61ddaef6b7f1474a7
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2010-03-13 21:10:59 +0000

    as3: typing_test: kończenie testu zaraz po przepisaniu ostatniego znaku

--- trunk/as3/typing_test/web/logview.php
+++ trunk/as3/typing_test/web/logview.php
@@ -21,11 +21,16 @@ if ($_SERVER['PHP_AUTH_USER'] != $username
 <html>
   <head>
     <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+    <style>
+      .logview-table td {
+        vertical-align: top;
+      }
+    </style>
   </head>
   <body>
     <h2>Log</h2>
 
-    <table cellspacing="8">
+    <table class="logview-table" cellspacing="8">
       <tr>
         <th>nr</th>
         <th>Data</th>
