390388a587f16cffadf2d17655cf134dd284f57c
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2010-03-14 10:54:30 +0000

    spnk: dodanie własnej funkcji obliczającej HMAC

--- spnk/tt/include/utils.php
+++ spnk/tt/include/utils.php
@@ -10,11 +10,26 @@ function rand_str($length) {
     return $str;
 }
 
-function isHMACValid($hash, $h_data, $h_key) {
+function hmac($data, $key) {
+    $hash_function = sha1;
+    if (!function_exists($hash_function)) {
+        return null;
+    }
+    $blocksize = 64;
+    if (strlen($key) > $blocksize) {
+        $key = $hash_function($key);
+    }
+    $key = str_pad($key, $blocksize, chr(0x00));
+    $opad = str_repeat(chr(0x5c), $blocksize) ^ $key;
+    $ipad = str_repeat(chr(0x36), $blocksize) ^ $key;
+    return $hash_function($opad . pack('H*', $hash_function($ipad . $data)));
+}
+
+function is_hmac_valid($hash, $h_data, $h_key) {
     if (empty($hash) || empty($h_data)) {
         return false;
     }
-    $valid_hash = hash_hmac('sha256', $h_data, $h_key);
+    $valid_hash = hmac($h_data, $h_key);
     return $hash == $valid_hash;
 }
 
