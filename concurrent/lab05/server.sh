c56a8e5d037f6d98eaa08842d97c40fb3eec6c30
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2010-03-23 16:08:03 +0000

    concurrent: lab05 początkowa wersja, lab06 treść zadania

---
+++ concurrent/lab05/server.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ ! -p ~/tmp/serwerfifo ]
+then
+  mkfifo ~/tmp/serwerfifo
+fi
+while true
+do
+  client_msg=`cat < ~/tmp/serwerfifo`
+  IFS=":"
+  tokens=($client_msg)
+  client_home=${tokens[0]}
+  number=${tokens[1]}
+  ./server_worker.sh $number $client_home &
+done
