f5cf6a112242d4d8d4456685fff1f2dd663b4756
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2010-03-23 19:42:40 +0000

    concurrent: lab05: przechwytywanie sygnałów

--- concurrent/lab05/client.sh
+++ concurrent/lab05/client.sh
@@ -3,19 +3,20 @@
 if [ $# -lt 2 ]
 then
   echo "Użycie: ./client.sh LICZBA NAZWA_KONTA_SERWERA"
-else
-  number=$1
-  server_home=$2
-  if [ ! -p /home/studinf/$server_home/tmp/serwerfifo ]
-  then
-    echo "Brak połączenia z serwerem"
-    exit 1
-  fi
-  if [ ! -p ~/tmp/klientfifo ]
-  then
-    mkfifo ~/tmp/klientfifo
-  fi
-  echo $HOME:$number > /home/studinf/$server_home/tmp/serwerfifo
-  result=`cat < ~/tmp/klientfifo`
-  echo "wynik: $result"
+  exit 2
 fi
+
+number=$1
+server_home=$2
+if [ ! -p /home/studinf/$server_home/tmp/serwerfifo ]
+then
+  echo "Brak połączenia z serwerem"
+  exit 1
+fi
+if [ ! -p ~/tmp/klientfifo ]
+then
+  mkfifo ~/tmp/klientfifo
+fi
+echo $HOME:$number > /home/studinf/$server_home/tmp/serwerfifo
+result=`cat < ~/tmp/klientfifo`
+echo "wynik: $result"
