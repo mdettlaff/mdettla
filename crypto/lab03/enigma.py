a4d1ba8db1b41729a74b239b81e99f39f6229c50
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2010-04-04 12:48:44 +0000

    crypto: symulator Enigmy z dowolną ilością rotorów

--- crypto/lab03/enigma.py
+++ crypto/lab03/enigma.py
@@ -1,84 +1,94 @@
 #!/usr/bin/env python
 # -*- encoding: UTF-8 -*-
 
-from string import ascii_uppercase as chars
+from string import ascii_uppercase as CHARS
 import doctest
 import random
 import sys
 
 
 class Enigma:
-    u"""Enigma z tylko jednym rotorem."""
-
-    def __init__(self, rotor, reflector):
-        self.rotor = rotor
+    def __init__(self, rotors, reflector):
+        self.rotors = rotors
         self.reflector = reflector
 
     def encrypt(self, input):
-        output = ''
-        for c in input:
-            self.rotor.rotate()
-            c = self.rotor.translate_forward(c)
-            c = self.reflector.translate(
-                    chars[(chars.index(c) - self.rotor.rotation) % len(chars)])
-            print 'po odbiciu:', c
-            c = chars[(chars.index(c) + self.rotor.rotation) % len(chars)]
-            c = self.rotor.translate_backward(c)
-            output += c
-        return output
+        return ''.join([self.__encrypt_char(c) for c in input])
+
+    def __rotate(self, rotors):
+        if rotors[0].rotation == len(rotors[0]) - 1 and len(rotors) > 1:
+            __rotate(rotors[1:])
+        rotors[0].rotate()
+
+    def __encrypt_char(self, c):
+        self.__rotate(self.rotors)
+        position = CHARS.index(c)
+        for rotor in self.rotors:
+            position = rotor.out_position_forward(position)
+        position = self.reflector.out_position(position)
+        for rotor in reversed(self.rotors):
+            position = rotor.out_position_backward(position)
+        return CHARS[position]
 
 class Rotor:
-    def __init__(self, permutation):
+    def __init__(self, permutation, initial_rotation):
         self.permutation = permutation
-        self.rotation = 0
+        self.rotation = initial_rotation
 
     def rotate(self):
-        self.rotation += 1
+        self.rotation = (self.rotation + 1) % len(self)
 
-    def translate_forward(self, c):
-        return self.permutation[(chars.index(c) + self.rotation) % len(chars)]
+    def out_position_forward(self, in_position):
+        return (self.permutation[(in_position + self.rotation) \
+                % len(self)] - self.rotation) % len(self)
 
-    def translate_backward(self, c):
-        return chars[(self.permutation.index(c) - self.rotation) % len(chars)]
+    def out_position_backward(self, in_position):
+        return (self.permutation.index((in_position + self.rotation) \
+                % len(self)) - self.rotation) % len(self)
+
+    def __len__(self):
+        return len(self.permutation)
 
 class Reflector:
     def __init__(self, involution):
-        self.permutation = involution
+            self.permutation = involution
 
-    def translate(self, c):
-        return self.permutation[chars.index(c)]
+    def out_position(self, in_position):
+        return self.permutation[in_position]
 
 
-def permutation():
-    return random.sample(chars, len(chars))
+def permutation(sequence):
+    return random.sample(sequence, len(sequence))
 
-def involution():
+def involution(sequence):
     u"""
-    >>> invol = involution()
-    >>> all([invol[chars.index(invol[chars.index(c)])] == c for c in chars])
+    >>> invol = involution(range(len(CHARS)))
+    >>> all([invol[invol[i]] == i for i in range(len(CHARS))])
     True
     """
-    sample = random.sample(chars, len(chars))
-    invol = [None for i in range(len(chars))]
+    sample = random.sample(sequence, len(sequence))
+    invol = [None for i in range(len(sequence))]
     for i in range(0, len(sample), 2):
-        invol[chars.index(sample[i])] = sample[i + 1]
-        invol[chars.index(sample[i + 1])] = sample[i]
+        invol[sequence.index(sample[i])] = sample[i + 1]
+        invol[sequence.index(sample[i + 1])] = sample[i]
     return invol
 
 def main():
     input = ''.join([line.strip().upper() for line in sys.stdin.readlines()])
-    perm = permutation()
-    invol = involution()
-    print '   ', [c for c in chars]
-    print 'N =', perm
-    print 'Z =', invol
+    N = permutation(range(len(CHARS)))
+    Z = involution(range(len(CHARS)))
 
-    enigma = Enigma(Rotor(perm), Reflector(invol))
+    print u'Enigma bez łącznicy NZN^1'
+#    print '   ', [c for c in CHARS]
+    print 'N =', [CHARS[i] for i in N]
+    print 'Z =', [CHARS[i] for i in Z]
+
+    enigma = Enigma([Rotor(N, 0)], Reflector(Z))
     encrypted = enigma.encrypt(input)
-    print 'po zaszyfrowaniu:', encrypted
-    enigma = Enigma(Rotor(perm), Reflector(invol))
+    print u'po zaszyfrowaniu:', encrypted
+    enigma = Enigma([Rotor(N, 0)], Reflector(Z))
     decrypted = enigma.encrypt(encrypted)
-    print 'po odszyfrowaniu:', decrypted
+    print u'po odszyfrowaniu:', decrypted
 
 if __name__ == '__main__':
     doctest.testmod()
