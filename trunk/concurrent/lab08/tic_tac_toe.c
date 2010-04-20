d2a7c9554abd98d0d29906c196b51af1d8a27792
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2010-04-20 18:52:23 +0000

    concurrent: lab08: prawie gotowe

--- trunk/concurrent/lab08/tic_tac_toe.c
+++ trunk/concurrent/lab08/tic_tac_toe.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
 #include <sys/shm.h>
 
+#define TRUE 1
+#define FALSE 0
+#define BOARD_SIZE 9
+#define EMPTY_CELL '.'
 #define SHM_KEY 1235 /* klucz pamięci współdzielonej */
-#define SEM_KEY 1111 /* klucz semafora */
+#define SEM_KEY 1116 /* klucz semafora */
 
 
 struct shared_use_st {
-    char buffer[BUFSIZ];
+    char board[BOARD_SIZE];
 };
 
 union semun {
@@ -25,13 +28,13 @@ union semun {
 
 // zmienne globalne
 int sem_id;
-int shmid;
+int shm_id;
 void *shared_memory;
 
 
-/*=================================================================*/
-/* Funkcje stanowiące uproszczony interfejs semaforów (binarnych). */
-/*=================================================================*/
+/*===========================================================================*/
+/* Funkcje stanowiące uproszczony interfejs semaforów (binarnych).           */
+/*===========================================================================*/
 
 /*
  * The function set_semvalue initializes the semaphore using the SETVAL command
@@ -54,7 +57,9 @@ static void del_semvalue(int sem_id) {
         fprintf(stderr, "Failed to delete semaphore\n");
 }
 
-/* Semaphore_p changes the semaphore by "1" This is the "wait" operation. */
+/*
+ * semaphore_p changes the semaphore by -1. This is the "wait" operation.
+ */
 static int semaphore_p(int sem_id) {
     struct sembuf sem_b;
     sem_b.sem_num = 0;
@@ -68,7 +73,7 @@ static int semaphore_p(int sem_id) {
 }
 
 /*
- * Semaphore_v is similar except for setting the sem_op part of the sembuf
+ * semaphore_v is similar except for setting the sem_op part of the sembuf
  * structure to 1. This is the "release" operation, so that the semaphore
  * becomes available.
  */
@@ -84,95 +89,143 @@ static int semaphore_v(int sem_id) {
     return 1;
 }
 
-/*=================================================================*/
+/*===========================================================================*/
 
 
-void clean_up() {
-    /* pozbywamy się pamięci współdzielonej */
-    printf("Odłączam pamięć współdzieloną... ");
-    if (shmdt(shared_memory) == -1) {
-        fprintf(stderr, "shmdt failed\n");
-        exit(EXIT_FAILURE);
-    } else {
-        printf("gotowe\n");
-    }
-    if (shmctl(shmid, IPC_RMID, 0) == -1) {
-        fprintf(stderr, "shmctl(IPC_RMID) failed\n");
+void init_shared_memory() {
+    int i;
+    shm_id = shmget((key_t)SHM_KEY, sizeof(struct shared_use_st),
+            0666 | IPC_CREAT);
+    if (shm_id == -1) {
+        fprintf(stderr, "shmget failed\n");
         exit(EXIT_FAILURE);
     }
-    /* pozbywamy się semafów */
-    printf("Usuwam semafor\n");
-    del_semvalue(sem_id);
-}
-
-void print_shared_memory(char* buffer) {
-    printf("On powiedział: %s\n", buffer);
-    if (strcmp(buffer, "koniec") == 0) {
-        clean_up();
-        exit(EXIT_SUCCESS);
-    }
-}
-
-void write_shared_memory(char* buffer) {
-    printf("Ja mówię: ");
-    scanf("%s", buffer);
-    if (strcmp(buffer, "koniec") == 0) {
-        clean_up();
-        exit(EXIT_SUCCESS);
-    }
-}
-
-void concurrent_block(char* who) {
-    shared_memory = shmat(shmid, (void *)0, 0);
+    shared_memory = shmat(shm_id, (void *)0, 0);
     if (shared_memory == (void *)-1) {
         fprintf(stderr, "shmat failed\n");
         exit(EXIT_FAILURE);
     }
-    printf("Memory attached at %X\n", (int)shared_memory);
-    struct shared_use_st *shared_stuff;
-    shared_stuff = (struct shared_use_st *)shared_memory;
-    while (1) {
-        if (!semaphore_p(sem_id)) {
+    //printf("memory attached at %X\n", (int)shared_memory);
+    struct shared_use_st *shared_variables;
+    shared_variables = (struct shared_use_st *)shared_memory;
+    char* board = shared_variables->board;
+    for (i = 0; i < BOARD_SIZE; i++) {
+        board[i] = EMPTY_CELL;
+    }
+}
+
+int init_semaphore() {
+    int player_id; // graczem 1 zostaje ten, kto pierwszy utworzył semafor
+    if ((sem_id = semget((key_t)SEM_KEY, 1, 0666 | IPC_CREAT | IPC_EXCL))
+            != -1) {
+        if (!set_semvalue(sem_id)) {
+            fprintf(stderr, "Failed to initialize semaphore\n");
             exit(EXIT_FAILURE);
         }
-        printf("%s: ", who);
-        print_shared_memory(shared_stuff->buffer);
-        printf("%s: ", who);
-        write_shared_memory(shared_stuff->buffer);
-        if (!semaphore_v(sem_id)) {
-            exit(EXIT_FAILURE);
+        player_id = 1;
+    } else {
+        sem_id = semget((key_t)SEM_KEY, 1, 0666 | IPC_CREAT);
+        player_id = 2;
+    }
+    return player_id;
+}
+
+void clean_up() {
+    /* pozbywamy się pamięci współdzielonej */
+    printf("odłączam pamięć współdzieloną... ");
+    if (shmdt(shared_memory) == -1) {
+        fprintf(stderr, "shmdt failed\n");
+        exit(EXIT_FAILURE);
+    } else {
+        printf("OK\n");
+    }
+    if (shmctl(shm_id, IPC_RMID, 0) == -1) {
+        fprintf(stderr, "shmctl(IPC_RMID) failed\n");
+        exit(EXIT_FAILURE);
+    }
+    /* pozbywamy się semafów */
+    printf("usuwam semafor\n");
+    del_semvalue(sem_id);
+}
+
+char get_player_mark(int player_id) {
+    return player_id == 1 ? 'X' : 'O';
+}
+
+int is_board_full(char* board) {
+    return FALSE; // TODO
+}
+
+int are_three_marks_in_a_row(char mark, char* board) {
+    return board[0] == mark; // TODO
+}
+
+void check_winner(char* board, int player_id) {
+    int winner;
+    if (are_three_marks_in_a_row(get_player_mark(1), board)) {
+        winner = 1; // wygrał gracz 1
+    } else if (are_three_marks_in_a_row(get_player_mark(2), board)) {
+        winner = 2; // wygrał gracz 2
+    } else if (is_board_full(board)) {
+        winner = 0; // remis
+    } else  {
+        winner = -1; // gra toczy się dalej
+    }
+    if (winner > 0) {
+        printf("wygrał gracz %d\n", winner);
+        if (player_id != winner) {
+            clean_up(); // przegrany musi posprzątać
         }
+        exit(EXIT_SUCCESS);
+    }
+}
+
+void print_board(char* board) {
+    int i;
+    for (i = 0; i < BOARD_SIZE; i++) {
+        if (i % 3 == 0) {
+            printf("\n");
+        }
+        printf(" %c", board[i]);
+    }
+    printf("\n");
+}
+
+void write_move(int position, char* board, int player_id) {
+    board[position] = get_player_mark(player_id);
+}
+
+void play_tic_tac_toe(int player_id) {
+    int other_player_id = 3 - player_id;
+    struct shared_use_st* shared_variables;
+    shared_variables = (struct shared_use_st*)shared_memory;
+    if (player_id != 1) {
+        print_board(shared_variables->board);
+    }
+    while (TRUE) {
+        if (!semaphore_p(sem_id)) exit(EXIT_FAILURE);
+        check_winner(shared_variables->board, player_id);
+        print_board(shared_variables->board);
+        printf("gracz %d: ", player_id);
+        printf("podaj pozycję: ");
+        int position;
+        scanf("%d", &position);
+        write_move(position, shared_variables->board, player_id);
+        print_board(shared_variables->board);
+        check_winner(shared_variables->board, player_id);
+        printf("oczekiwanie na ruch gracza %d...\n", other_player_id);
+        if (!semaphore_v(sem_id)) exit(EXIT_FAILURE);
     }
 }
 
 int main() {
-    /* inicjalizacja semafora */
-    sem_id = semget((key_t)SEM_KEY, 1, 0666 | IPC_CREAT);
-    if (!set_semvalue(sem_id)) {
-        fprintf(stderr, "Failed to initialize semaphore\n");
-        exit(EXIT_FAILURE);
-    }
+    int player_id;
 
-    /* inicjalizacja pamięci współdzielonej */
-    shared_memory = (void *)0;
-    shmid = shmget((key_t)SHM_KEY, sizeof(struct shared_use_st),
-            0666 | IPC_CREAT);
-    if (shmid == -1) {
-        fprintf(stderr, "shmget failed\n");
-        exit(EXIT_FAILURE);
-    }
+    init_shared_memory();
+    // graczem 1 zostaje ten, kto pierwszy utworzył semafor
+    player_id = init_semaphore();
 
-    switch (fork()) {
-        case -1: /* błąd */
-            perror("Wystapil błąd przy tworzeniu procesu");
-            exit(1);
-            break;
-        case 0: /* proces potomny */
-            concurrent_block("Proces potomny");
-            break;
-        default: /* rodzic */
-            concurrent_block("Proces rodzicielski");
-            break;
-    }
+    play_tic_tac_toe(player_id);
+
     return EXIT_SUCCESS;
 }
