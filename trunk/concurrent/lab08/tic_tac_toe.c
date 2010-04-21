4fd71f88ed2d92fea3f6eb443dfaaf62dd1b964a
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2010-04-21 20:11:49 +0000

    concurrent: lab09: plansza jako tablica dwuwymiarowa, wydzielenie API dla semaforów do osobnego pliku

--- trunk/concurrent/lab08/tic_tac_toe.c
+++ trunk/concurrent/lab08/tic_tac_toe.c
@@ -6,23 +6,18 @@
 #include <sys/sem.h>
 #include <sys/shm.h>
 
+#include "binary_sem.h"
+
 #define TRUE 1
 #define FALSE 0
-#define BOARD_SIZE 9
-#define EMPTY_CELL '.'
-#define SHM_KEY 1235 /* klucz pamięci współdzielonej */
+#define BOARD_SIZE 3 /* plansza ma wymiary BOARD_SIZE x BOARD_SIZE */
+#define EMPTY_CELL ' '
+#define SHM_KEY 1236 /* klucz pamięci współdzielonej */
 #define SEM_KEY 1116 /* klucz semafora */
 
 
 struct shared_use_st {
-    char board[BOARD_SIZE];
-};
-
-union semun {
-    int val;
-    struct semid_ds *buf;
-    unsigned short *array;
-    struct seminfo *__buf;
+    char board[BOARD_SIZE][BOARD_SIZE];
 };
 
 
@@ -32,68 +27,7 @@ int shm_id;
 void *shared_memory;
 
 
-/*===========================================================================*/
-/* Funkcje stanowiące uproszczony interfejs semaforów (binarnych).           */
-/*===========================================================================*/
-
-/*
- * The function set_semvalue initializes the semaphore using the SETVAL command
- * in a semctl call. You need to do this before you can use the semaphore.
- */
-static int set_semvalue(int sem_id) {
-    union semun sem_union;
-    sem_union.val = 1;
-    if (semctl(sem_id, 0, SETVAL, sem_union) == -1) return 0;
-    return 1;
-}
-
-/*
- * The del_semvalue function has almost the same form, except that the call
- * to semctl uses the command IPC_RMID to remove the semaphore's ID.
- */
-static void del_semvalue(int sem_id) {
-    union semun sem_union;
-    if (semctl(sem_id, 0, IPC_RMID, sem_union) == -1)
-        fprintf(stderr, "Failed to delete semaphore\n");
-}
-
-/*
- * semaphore_p changes the semaphore by -1. This is the "wait" operation.
- */
-static int semaphore_p(int sem_id) {
-    struct sembuf sem_b;
-    sem_b.sem_num = 0;
-    sem_b.sem_op = -1; /* P() */
-    sem_b.sem_flg = SEM_UNDO;
-    if (semop(sem_id, &sem_b, 1) == -1) {
-        fprintf(stderr, "semaphore_p failed\n");
-        return 0;
-    }
-    return 1;
-}
-
-/*
- * semaphore_v is similar except for setting the sem_op part of the sembuf
- * structure to 1. This is the "release" operation, so that the semaphore
- * becomes available.
- */
-static int semaphore_v(int sem_id) {
-    struct sembuf sem_b;
-    sem_b.sem_num = 0;
-    sem_b.sem_op = 1; /* V() */
-    sem_b.sem_flg = SEM_UNDO;
-    if (semop(sem_id, &sem_b, 1) == -1) {
-        fprintf(stderr, "semaphore_v failed\n");
-        return 0;
-    }
-    return 1;
-}
-
-/*===========================================================================*/
-
-
 void init_shared_memory() {
-    int i;
     shm_id = shmget((key_t)SHM_KEY, sizeof(struct shared_use_st),
             0666 | IPC_CREAT);
     if (shm_id == -1) {
@@ -108,9 +42,11 @@ void init_shared_memory() {
     //printf("memory attached at %X\n", (int)shared_memory);
     struct shared_use_st *shared_variables;
     shared_variables = (struct shared_use_st *)shared_memory;
-    char* board = shared_variables->board;
+    int i, j;
     for (i = 0; i < BOARD_SIZE; i++) {
-        board[i] = EMPTY_CELL;
+        for (j = 0; j < BOARD_SIZE; j++) {
+            (shared_variables->board)[i][j] = EMPTY_CELL;
+        }
     }
 }
 
@@ -152,15 +88,23 @@ char get_player_mark(int player_id) {
     return player_id == 1 ? 'X' : 'O';
 }
 
-int is_board_full(char* board) {
-    return FALSE; // TODO
+int is_board_full(char board[BOARD_SIZE][BOARD_SIZE]) {
+    int i, j;
+    for (i = 0; i < BOARD_SIZE; i++) {
+        for (j = 0; j < BOARD_SIZE; j++) {
+            if (board[i][j] == EMPTY_CELL) {
+                return FALSE;
+            }
+        }
+    }
+    return TRUE;
 }
 
-int are_three_marks_in_a_row(char mark, char* board) {
-    return board[0] == mark; // TODO
+int are_three_marks_in_a_row(char mark, char board[BOARD_SIZE][BOARD_SIZE]) {
+    return board[0][0] == mark; // TODO
 }
 
-void check_winner(char* board, int player_id) {
+void check_winner(char board[BOARD_SIZE][BOARD_SIZE], int player_id) {
     int winner;
     if (are_three_marks_in_a_row(get_player_mark(1), board)) {
         winner = 1; // wygrał gracz 1
@@ -180,37 +124,50 @@ void check_winner(char* board, int player_id) {
     }
 }
 
-void print_board(char* board) {
-    int i;
-    for (i = 0; i < BOARD_SIZE; i++) {
-        if (i % 3 == 0) {
-            printf("\n");
-        }
-        printf(" %c", board[i]);
-    }
-    printf("\n");
+void write_move(int row, int column, char board[BOARD_SIZE][BOARD_SIZE],
+        int player_id) {
+    board[row - 1][column - 1] = get_player_mark(player_id);
 }
 
-void write_move(int position, char* board, int player_id) {
-    board[position] = get_player_mark(player_id);
+void print_board(char board[BOARD_SIZE][BOARD_SIZE]) {
+    int i, j;
+    for (i = 0; i < BOARD_SIZE; i++) {
+        if (i != 0) {
+            for (j = 0; j < BOARD_SIZE; j++) {
+                if (j != 0) {
+                    printf("+");
+                }
+                printf("-");
+            }
+            printf("\n");
+        }
+        for (j = 0; j < BOARD_SIZE; j++) {
+            if (j != 0) {
+                printf("|");
+            }
+            printf("%c", board[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 void play_tic_tac_toe(int player_id) {
-    int other_player_id = 3 - player_id;
     struct shared_use_st* shared_variables;
     shared_variables = (struct shared_use_st*)shared_memory;
+    int other_player_id = 3 - player_id;
     if (player_id != 1) {
         print_board(shared_variables->board);
+        printf("oczekiwanie na ruch gracza %d...\n", other_player_id);
     }
     while (TRUE) {
         if (!semaphore_p(sem_id)) exit(EXIT_FAILURE);
         check_winner(shared_variables->board, player_id);
         print_board(shared_variables->board);
         printf("gracz %d: ", player_id);
-        printf("podaj pozycję: ");
-        int position;
-        scanf("%d", &position);
-        write_move(position, shared_variables->board, player_id);
+        printf("podaj pozycję rząd, kolumna: ");
+        int row, column;
+        scanf("%d %d", &row, &column);
+        write_move(row, column, shared_variables->board, player_id);
         print_board(shared_variables->board);
         check_winner(shared_variables->board, player_id);
         printf("oczekiwanie na ruch gracza %d...\n", other_player_id);
@@ -219,11 +176,9 @@ void play_tic_tac_toe(int player_id) {
 }
 
 int main() {
-    int player_id;
-
     init_shared_memory();
     // graczem 1 zostaje ten, kto pierwszy utworzył semafor
-    player_id = init_semaphore();
+    int player_id = init_semaphore();
 
     play_tic_tac_toe(player_id);
 
