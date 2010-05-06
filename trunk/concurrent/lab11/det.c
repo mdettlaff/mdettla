3bcf216cbb9cf034c1816cec4ef67d585d28d0df
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2010-05-06 18:36:23 +0000

    concurrent: lab11, rekurencyjne wywoływanie wątków

--- trunk/concurrent/lab11/det.c
+++ trunk/concurrent/lab11/det.c
@@ -1,119 +1,178 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
 
+typedef struct matrix_struct {
+    double **a; // elementy
+    int dim; // wymiar
+    double det; // wyznacznik
+} matrix_t;
+
+
 /*
  * Prosi użytkownika o podanie macierzy z klawiatury i zwraca wskaźnik
- * do niej, a do zmiennej matrix_dimension przypisuje wymiar macierzy.
+ * do niej.
  */
-double **read_matrix(int *matrix_dimension) {
-    double **matrix;
+matrix_t *read_matrix() {
+    matrix_t *matrix;
     int i, j;
+    matrix = malloc(sizeof (matrix_t));
     printf("Podaj wymiar macierzy:\n");
-    scanf("%d", matrix_dimension);
+    scanf("%d", &matrix->dim);
     printf("Podaj zawartość macierzy:\n");
-    matrix = malloc(*matrix_dimension * sizeof (*matrix));
-    for (i = 0; i < *matrix_dimension; i++) {
-        matrix[i] = malloc(*matrix_dimension * sizeof (**matrix));
-        for (j = 0; j < *matrix_dimension; j++) {
-            scanf("%lf", &matrix[i][j]);
+    matrix->a = malloc(matrix->dim * sizeof (*matrix->a));
+    for (i = 0; i < matrix->dim; i++) {
+        matrix->a[i] = malloc(matrix->dim * sizeof (**matrix->a));
+        for (j = 0; j < matrix->dim; j++) {
+            scanf("%lf", &matrix->a[i][j]);
         }
     }
     return matrix;
 }
 
-void print_matrix(double **matrix, int dimension) {
+void print_matrix(matrix_t *matrix) {
     int i, j;
-    for (i = 0; i < dimension; i++) {
-        for (j = 0; j < dimension; j++) {
-            printf("%.1lf ", matrix[i][j]);
+    for (i = 0; i < matrix->dim; i++) {
+        for (j = 0; j < matrix->dim; j++) {
+            printf("%.1lf ", matrix->a[i][j]);
         }
         printf("\n");
     }
 }
 
 /*
- * Oblicza wyznacznik podanej macierzy, jeśli jej rozmiar (dimension) jest
+ * Oblicza wyznacznik podanej macierzy, jeśli jej wymiar (dim) jest
  * równy 2 lub 1 (trywialne przypadki).
  */
-double det_trivial(double **matrix, int dimension) {
-    if (dimension == 1) {
-        return matrix[0][0];
+double det_trivial(matrix_t *matrix) {
+    if (matrix->dim == 1) {
+        return matrix->a[0][0];
     }
-    return matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1];
+    return matrix->a[0][0] * matrix->a[1][1] - matrix->a[1][0] * matrix->a[0][1];
 }
 
 /*
  * Oblicza wyznacznik podanej macierzy 3x3 metodą Sarrusa.
  */
-double det_sarrus(double **matrix) {
+double det_sarrus(matrix_t *A) {
     int i;
     double det = 0;
     for (i = 0; i < 3; i++) {
-        det +=
-            matrix[0][i % 3] * matrix[1][(i + 1) % 3] * matrix[2][(i + 2) % 3];
+        det += A->a[0][i % 3] * A->a[1][(i + 1) % 3] * A->a[2][(i + 2) % 3];
     }
     for (i = 0; i < 3; i++) {
-        det -=
-            matrix[2][i % 3] * matrix[1][(i + 1) % 3] * matrix[0][(i + 2) % 3];
+        det -= A->a[2][i % 3] * A->a[1][(i + 1) % 3] * A->a[0][(i + 2) % 3];
     }
     return det;
 }
 
 /*
- * Zwraca macierz powstałą z macierzy matrix poprzez skreślenie z niej
- * wiersza row oraz kolumny column. Macierz matrix ma rozmiar
- * dimension * dimension, a zwracana macierz (dimension - 1) * (dimension - 1).
+ * Tworzy macierz powstałą z macierzy matrix poprzez skreślenie z niej
+ * wiersza i oraz kolumny j, po czym zwraca wskaźnik do niej. Jeśli macierz
+ * matrix jest wymiaru n, to zwracana macierz jest wymiaru n-1.
  */
-double **create_minor(double **matrix, int dimension, int row, int column) {
-    int i, j, ii, jj;
-    double **minor = malloc((dimension - 1) * sizeof (*minor));
-    for (i = 0; i < dimension - 1; i++) {
-        minor[i] = malloc((dimension - 1) * sizeof (**minor));
-        ii = i < row ? i : i + 1;
-        for (j = 0; j < dimension - 1; j++) {
-            jj = j < column ? j : j + 1;
-            minor[i][j] = matrix[ii][jj];
+matrix_t *create_minor(matrix_t *matrix, int i, int j) {
+    matrix_t *minor;
+    int i_minor, j_minor, i_matrix, j_matrix;
+    minor = malloc(sizeof (matrix_t));
+    minor->dim = matrix->dim - 1;
+    minor->a = malloc(minor->dim * sizeof (*minor->a));
+    for (i_minor = 0; i_minor < minor->dim; i_minor++) {
+        minor->a[i_minor] = malloc(minor->dim * sizeof (**minor->a));
+        i_matrix = i_minor < i ? i_minor : i_minor + 1;
+        for (j_minor = 0; j_minor < minor->dim; j_minor++) {
+            j_matrix = j_minor < j ? j_minor : j_minor + 1;
+            minor->a[i_minor][j_minor] = matrix->a[i_matrix][j_matrix];
         }
     }
     return minor;
 }
 
 /*
- * Oblicza rekurencyjnie wyznacznik podanej macierzy z rozwinięcia Laplace'a.
+ * Wątek, który bierze jako argument wejściowy wskaźnik do macierzy
+ * i wpisuje w jej polu ->det obliczoną wartość wyznacznika tej macierzy.
+ */
+void *det_laplace_thread(void *arg) {
+    matrix_t *matrix;
+    int k;
+    int res;
+    matrix = (matrix_t *)arg;
+    if (matrix->dim > 3) {
+        // tworzymy minory macierzy potrzebne do obliczenia rozwinięcia
+        // Laplace'a według pierwszego wiersza
+        matrix_t *minors[matrix->dim];
+        for (k = 0; k < matrix->dim; k++) {
+            minors[k] = create_minor(matrix, 0, k);
+        }
+
+        /*
+         * Obliczamy rekurencyjnie wyznaczniki minorów danej macierzy.
+         * Wyznacznik każdego minora obliczany jest przez osobny wątek
+         * (nową instancję tego wątku).
+         */
+        pthread_t threads[matrix->dim];
+        for (k = 0; k < matrix->dim; k++) {
+            res = pthread_create(
+                    &threads[k], NULL, det_laplace_thread, (void *)minors[k]);
+            if (res != 0) {
+                perror("Thread creation failed");
+                exit(EXIT_FAILURE);
+            }
+        }
+        for (k = 0; k < matrix->dim; k++) {
+            void *thread_result;
+            res = pthread_join(threads[k], &thread_result);
+            if (res != 0) {
+                perror("Thread join failed");
+                exit(EXIT_FAILURE);
+            }
+        }
+        // po tym jak wątki zakończyły działanie, każdy minor ma wpisany
+        // swój wyznacznik w polu ->det
+
+        matrix->det = 0;
+        for (k = 0; k < matrix->dim; k++) {
+            double cofactor = pow(-1, 0 + k) * minors[k]->det;
+            // stosujemy rozwinięcie według pierwszego wiersza
+            matrix->det += matrix->a[0][k] * cofactor;
+        }
+    } else if (matrix->dim == 3) {
+        matrix->det = det_sarrus(matrix);
+    } else {
+        matrix->det = det_trivial(matrix);
+    }
+    pthread_exit(NULL);
+}
+
+/*
+ * Zwraca wyznacznik podanej macierzy obliczony z rozwinięcia Laplace'a.
  * Jeśli macierz ma rozmiar 3x3 lub mniejszy, do obliczenia wyznacznika
  * stosowane są prostsze metody (np. reguła Sarrusa).
  */
-double det_laplace(double **matrix, int dimension) {
-    int k;
-    if (dimension > 3) {
-        double det = 0;
-        // rozwinięcie według pierwszego wiersza
-        for (k = 0; k < dimension; k++) {
-            double **minor = create_minor(matrix, dimension, 0, k);
-            double cofactor =
-                pow(-1, 0 + k) * det_laplace(minor, dimension - 1);
-            det += matrix[0][k] * cofactor;
-        }
-        return det;
-    } else if (dimension == 3) {
-        return det_sarrus(matrix);
-    } else {
-        return det_trivial(matrix, dimension);
+double det_laplace(matrix_t *matrix) {
+    pthread_t thread;
+    void *thread_result;
+    int res = pthread_create(
+            &thread, NULL, det_laplace_thread, (void *)matrix);
+    if (res != 0) {
+        perror("Thread creation failed");
+        exit(EXIT_FAILURE);
     }
+    res = pthread_join(thread, &thread_result);
+    if (res != 0) {
+        perror("Thread join failed");
+        exit(EXIT_FAILURE);
+    }
+    return matrix->det;
 }
 
 int main() {
-    double **matrix;
-    int matrix_dimension;
+    matrix_t *matrix;
 
-    matrix = read_matrix(&matrix_dimension);
-    printf("A =\n");
-    print_matrix(matrix, matrix_dimension);
+    matrix = read_matrix();
+    printf("wyznacznik macierzy jest równy %.1lf\n", det_laplace(matrix));
 
-    printf("det(A) = %.1lf\n", det_laplace(matrix, matrix_dimension));
-
-    free(matrix);
     return EXIT_SUCCESS;
 }
