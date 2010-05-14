bf83bd67a1e5129b2f789dd83168a47e4cacf839
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2010-05-14 12:09:33 +0000

    concurrent: projekt: przerwanie gry w dowolnym momencie

---
+++ trunk/concurrent/projekt/binary_sem.h
@@ -0,0 +1,34 @@
+#ifndef _binary_sem_h
+#define _binary_sem_h
+
+
+/*===========================================================================*/
+/* Funkcje stanowiące uproszczony interfejs semaforów (binarnych).           */
+/*===========================================================================*/
+
+/*
+ * The function set_semvalue initializes the semaphore using the SETVAL command
+ * in a semctl call. You need to do this before you can use the semaphore.
+ */
+int set_semvalue(int sem_id, int sem_num, int value);
+
+/*
+ * The del_semvalue function has almost the same form, except that the call
+ * to semctl uses the command IPC_RMID to remove the semaphore's ID.
+ */
+void del_semvalue(int sem_id);
+
+/*
+ * semaphore_p changes the semaphore by -1. This is the "wait" operation.
+ */
+int semaphore_p(int sem_id, int sem_num);
+
+/*
+ * semaphore_v is similar except for setting the sem_op part of the sembuf
+ * structure to 1. This is the "release" operation, so that the semaphore
+ * becomes available.
+ */
+int semaphore_v(int sem_id, int sem_num);
+
+
+#endif
