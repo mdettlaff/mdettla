def0267a550e7957cb041146ed581d9a1fe2514c
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2010-10-21 17:11:02 +0000

    haskell: dalsze zadania z lab03

--- haskell/lab03/zadania3.hs
+++ haskell/lab03/zadania3.hs
@@ -7,12 +7,23 @@ member x (y:ys) = if x == y then True else member x ys
 my_reverse [] = []
 my_reverse (x:xs) = append (my_reverse xs) [x]
 
+my_last [x] = x
+my_last (x:xs) = my_last xs
+
+delete x [] = []
+delete x (y:ys) = if x == y then delete x ys else y:(delete x (ys))
+
 my_map f [] = []
 my_map f (x:xs) = (f x):(my_map f xs)
+--my_map f = foldr ((:) . f) [] -- head == explode!
 
 square x = x * x
 -- przykład zastosowania currying
 --mapper f = \ xs -> my_map f xs
 mapper f = \ xs -> if xs == [] then [] else (f (head xs)):((mapper f) (tail xs))
 -- funkcja podnosząca do kwadratu każdy element podanej listy
-squarer = mapper square
+--squarer = mapper square
+squarer = my_map square
+
+iter 0 f = (\ x -> x)
+iter n f = f.(iter (n - 1) f)
