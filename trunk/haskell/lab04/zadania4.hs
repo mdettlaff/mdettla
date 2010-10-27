3f8b71097e2eec622bc80b49c0507d8ded217c78
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2010-10-27 21:23:54 +0000

    haskell: insertion_sort z użyciem foldr

--- trunk/haskell/lab04/zadania4.hs
+++ trunk/haskell/lab04/zadania4.hs
@@ -8,23 +8,28 @@ my_foldl f z (x:xs) = my_foldl f (f z x) xs
 my_foldr f z [] = z
 my_foldr f z (x:xs) = f x (my_foldr f z xs)
 
-my_length = my_foldl (\ x y -> x + 1) 0
-my_and = my_foldl (&&) True
-my_sum = my_foldl (+) 0
-my_prod = my_foldl (*) 1
-my_map f = my_foldr ((:) . f) []
-my_reverse = my_foldr (\ x y -> y ++ [x]) []
+my_length = foldl (\ x y -> x + 1) 0
+my_and = foldl (&&) True
+my_sum = foldl (+) 0
+my_prod = foldl (*) 1
+my_map f = foldr ((:) . f) []
+my_reverse = foldr (\ x y -> y ++ [x]) []
 
 my_zip_with f [] _ = []
 my_zip_with f (x:xs) (y:ys) = (f x y):(my_zip_with f xs ys)
 
-prod v1 v2 = my_foldl (+) 0 (my_zip_with (*) v1 v2)
+prod v1 v2 = foldl (+) 0 (my_zip_with (*) v1 v2)
 
 --flatten [] = []
 --flatten (x:xs) = x ++ (flatten xs)
-flatten = my_foldl (++) []
+flatten = foldl (++) []
 
-insertion_sort [] = []
-insertion_sort (x:xs) = insert x (insertion_sort xs)
-    where insert x xs = [y | y <- xs, y < x] ++ [x] ++ [y | y <- xs, x <= y]
+
+insert x xs = [y | y <- xs, y < x] ++ [x] ++ [y | y <- xs, x <= y]
+
+--insertion_sort [] = []
+--insertion_sort (x:xs) = insert x (insertion_sort xs)
+
+insertion_sort :: [Int] -> [Int]
+insertion_sort = foldr insert []
 
