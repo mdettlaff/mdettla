cadfc2dd72070e748a36f1a6f884cb179a4930bd
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2010-10-27 19:32:57 +0000

    Brakujące zadania z lab03 (użycie list comprehensions).

--- haskell/lab03/zadania3.hs
+++ haskell/lab03/zadania3.hs
@@ -10,8 +10,9 @@ my_reverse (x:xs) = append (my_reverse xs) [x]
 my_last [x] = x
 my_last (x:xs) = my_last xs
 
-delete x [] = []
-delete x (y:ys) = if x == y then delete x ys else y:(delete x (ys))
+--delete x [] = []
+--delete x (y:ys) = if x == y then delete x ys else y:(delete x (ys))
+delete x ys = [y | y <- ys, not (y == x)]
 
 my_map f [] = []
 my_map f (x:xs) = (f x):(my_map f xs)
@@ -24,6 +25,12 @@ mapper f = \ xs -> if xs == [] then [] else (f (head xs)):((mapper f) (tail xs))
 -- funkcja podnosząca do kwadratu każdy element podanej listy
 --squarer = mapper square
 squarer = my_map square
+
+permutations [] = [[]]
+permutations xs = [x:ys | x <- xs, ys <- (permutations (delete x xs))]
+
+pythagorean_triple n =
+    [(x, y, z) | x <- [1..n], y <- [1..n], z <- [1..n], x^2 + y^2 == z^2]
 
 iter 0 f = (\ x -> x)
 iter n f = f.(iter (n - 1) f)
