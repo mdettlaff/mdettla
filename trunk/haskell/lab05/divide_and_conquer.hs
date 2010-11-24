0822f314948cb200303601875c436e4edb7d5baf
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2010-11-24 19:58:14 +0000

    haskell: mergesort z użyciem funkcji dc (divide and conquer)

--- trunk/haskell/lab05/divide_and_conquer.hs
+++ trunk/haskell/lab05/divide_and_conquer.hs
@@ -1,8 +1,10 @@
+-- divide and conquer
 dc test end divide comb p =
-  if (test p)
-    then (end p)
-    else comb ((map (dc test end divide comb)
-                    (divide p)))
+  if test p
+    then end p
+    else comb (map (dc test end divide comb)
+                    (divide p))
+
 
 split [] = []
 split (a:as) =
@@ -12,4 +14,16 @@ split (a:as) =
 flatten = foldl (++) []
 
 quicksort :: Ord a => [a] -> [a]
-quicksort = dc (\ l -> length l <= 1) (\ x -> x) (split) (flatten)
+quicksort = dc (\ xs -> length xs <= 1) (\ x -> x) split flatten
+
+
+ident x = x
+halve xs = (\ pair -> [fst pair, snd pair]) (splitAt (div (length xs) 2) xs)
+
+merge [] xs = xs
+merge xs [] = xs
+merge (x:xs) (y:ys) =
+  if x < y then x:(merge xs (y:ys)) else y:(merge (x:xs) ys)
+
+mergesort :: Ord a => [a] -> [a]
+mergesort = dc (\ xs -> length xs <= 1) ident halve (\ [xs, ys] -> merge xs ys)
