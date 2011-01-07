adb25fe2b27af95de5197072cb503db33fd74030
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2011-01-07 14:28:53 +0000

    haskell: my_drop

--- haskell/lab06/maybe.hs
+++ haskell/lab06/maybe.hs
@@ -1,10 +1,15 @@
 -- Maybe t = Nothing | Just t
 
-f x Nothing = Nothing
-f Nothing y = Nothing
-f (Just x) (Just y) = Just (x + y)
+add x Nothing = Nothing
+add Nothing y = Nothing
+add (Just x) (Just y) = Just (x + y)
 
 pozycja :: Int -> [Int] -> Maybe Int
 pozycja x [] = Nothing
-pozycja x (y:ys) = if x == y then Just 0 else f (pozycja x ys) (Just 1)
+pozycja x (y:ys) = if x == y then Just 0 else add (pozycja x ys) (Just 1)
 
+my_drop :: Int -> [a] -> Maybe [a]
+my_drop 0 [] = Just []
+my_drop 0 (x:xs) = Just (x:xs)
+my_drop n [] = Nothing
+my_drop n (x:xs) = my_drop (n - 1) xs
