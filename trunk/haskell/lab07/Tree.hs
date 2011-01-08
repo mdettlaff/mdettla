90365ace8510bd4ef65ea2dd46f999fef5ebb2b2
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2011-01-08 13:41:50 +0000

    haskell: foldTree, mapTree, sizeTree, sumTree, preTree

--- trunk/haskell/lab07/Tree.hs
+++ trunk/haskell/lab07/Tree.hs
@@ -10,4 +10,13 @@ tree1 = Node 1 [Node 2 [], tree3, tree4]
 --    / \  \
 --    5 6   7
 
-sizeTree (Node n xs) = 1 + sum (map sizeTree xs)
+mapTree f (Node n xs) = Node (f n) (map (mapTree f) xs)
+
+foldTree g f (Node n xs) = foldl f (g n) (map (foldTree g f) xs)
+
+--sizeTree (Node n xs) = 1 + sum (map sizeTree xs)
+sizeTree = foldTree (\ x -> 1) (+)
+
+sumTree = foldTree (\ x -> x) (+)
+
+preTree = foldTree (\ x -> [x]) (++)
