3fa25dd0eec33fb7984a32a17702a873872e57e8
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2011-01-09 16:29:36 +0000

    haskell: showQueue, poprawienie enQueue i deQueue

--- trunk/haskell/lab07/Queue.hs
+++ trunk/haskell/lab07/Queue.hs
@@ -6,23 +6,24 @@ class Queue q where
     enQueue :: a -> q a -> q a
     deQueue :: q a -> q a
     getFrontQueue :: q a -> a
+    showQueue :: (Show a) => q a -> String
+    showQueue xs =
+        if isEmptyQueue xs then ""
+        else " -> " ++ (show (getFrontQueue xs)) ++ (showQueue (deQueue xs))
 
 instance Queue ConsQueue where
     isEmptyQueue EmptyQueue = True
     isEmptyQueue _ = False
-    enQueue x xs = Cons x xs
-    deQueue (Cons x EmptyQueue) = EmptyQueue
-    deQueue (Cons x xs) = Cons x (deQueue xs)
+    enQueue x EmptyQueue = Cons x EmptyQueue
+    enQueue x (Cons y ys) = Cons y (enQueue x ys)
+    deQueue (Cons x xs) = xs
     getFrontQueue (Cons x xs) = x
 
 instance Queue ListQueue where
     isEmptyQueue (LQ xs) = length xs == 0
-    enQueue x (LQ xs) = LQ (x:xs)
-    deQueue (LQ xs) = LQ (remove_last xs)
+    enQueue x (LQ xs) = LQ (xs ++ [x])
+    deQueue (LQ (x:xs)) = LQ (xs)
     getFrontQueue (LQ xs) = head xs
-
-remove_last [x] = []
-remove_last (x:xs) = x:(remove_last xs)
 
 q1 = EmptyQueue
 q2 = Cons 3 (Cons 1 (Cons 2 EmptyQueue))
