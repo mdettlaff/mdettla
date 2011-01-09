99640b5640153a23ff22092c7a0f939a16f154a1
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2011-01-09 14:46:18 +0000

    haskell: kolejki z użyciem konstruktorów

--- trunk/haskell/lab07/Queue.hs
+++ trunk/haskell/lab07/Queue.hs
@@ -1,18 +1,17 @@
 data ConsQueue a = EmptyQueue | Cons a (ConsQueue a) deriving Show
 
 class Queue q where
-    type Elem q :: *
-    isEmptyQueue :: q -> Bool
-    enQueue :: Elem q -> q -> q
-    deQueue :: q -> q
-    getFrontQueue :: q -> Elem q
+    isEmptyQueue :: q a -> Bool
+    enQueue :: a -> q a -> q a
+    deQueue :: q a -> q a
+    getFrontQueue :: q a -> a
 
-instance Queue (ConsQueue a) where
-    type Elem (ConsQueue a) = a
+instance Queue ConsQueue where
     isEmptyQueue EmptyQueue = True
     isEmptyQueue _ = False
     enQueue x xs = Cons x xs
-    deQueue (Cons x xs) = xs
+    deQueue (Cons x EmptyQueue) = EmptyQueue
+    deQueue (Cons x xs) = Cons x (deQueue xs)
     getFrontQueue (Cons x xs) = x
 
 
