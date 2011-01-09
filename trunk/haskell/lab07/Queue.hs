1e376e8553d4e52e0e6e422aaa31d79d4cf8e1c5
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2011-01-09 14:25:37 +0000

    haskell: operacje na kolejkach

--- trunk/haskell/lab07/Queue.hs
+++ trunk/haskell/lab07/Queue.hs
@@ -1,11 +1,19 @@
 data ConsQueue a = EmptyQueue | Cons a (ConsQueue a) deriving Show
 
-class Queue a where
-    isEmptyQueue :: a -> Bool
+class Queue q where
+    type Elem q :: *
+    isEmptyQueue :: q -> Bool
+    enQueue :: Elem q -> q -> q
+    deQueue :: q -> q
+    getFrontQueue :: q -> Elem q
 
 instance Queue (ConsQueue a) where
+    type Elem (ConsQueue a) = a
     isEmptyQueue EmptyQueue = True
     isEmptyQueue _ = False
+    enQueue x xs = Cons x xs
+    deQueue (Cons x xs) = xs
+    getFrontQueue (Cons x xs) = x
 
 
 q1 = EmptyQueue
