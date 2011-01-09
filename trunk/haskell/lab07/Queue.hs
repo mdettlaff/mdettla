c3f81b16df846a05238d1d3dfd761c8f2bd5bda8
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2011-01-09 15:45:04 +0000

    haskell: kolejka reprezentowana przez listę

--- trunk/haskell/lab07/Queue.hs
+++ trunk/haskell/lab07/Queue.hs
@@ -1,4 +1,5 @@
 data ConsQueue a = EmptyQueue | Cons a (ConsQueue a) deriving Show
+data ListQueue a = LQ [a] deriving Show
 
 class Queue q where
     isEmptyQueue :: q a -> Bool
@@ -14,6 +15,16 @@ instance Queue ConsQueue where
     deQueue (Cons x xs) = Cons x (deQueue xs)
     getFrontQueue (Cons x xs) = x
 
+instance Queue ListQueue where
+    isEmptyQueue (LQ xs) = length xs == 0
+    enQueue x (LQ xs) = LQ (x:xs)
+    deQueue (LQ xs) = LQ (remove_last xs)
+    getFrontQueue (LQ xs) = head xs
+
+remove_last [x] = []
+remove_last (x:xs) = x:(remove_last xs)
 
 q1 = EmptyQueue
 q2 = Cons 3 (Cons 1 (Cons 2 EmptyQueue))
+lq1 = LQ []
+lq2 = LQ [3, 1, 2]
