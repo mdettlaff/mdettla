ef87afa216d534de12fa60a9f7031f358281dec1
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2011-01-10 10:01:24 +0000

    haskell: TreeSet, deleteTree

---
+++ haskell/lab07/TreeSet.hs
@@ -0,0 +1,14 @@
+import Tree
+import Set
+
+data TreeSet a = Tree (Set a) [Tree (Set a)]
+
+ts1 = Node (Cons 1 EmptySet) []
+ts2 =
+    Node (Cons 1 (Cons 2 EmptySet))
+        [Node (Cons 2 (Cons 3 EmptySet))
+            [Node (Cons 2 (Cons 1 (Cons 5 EmptySet))) []],
+         Node (Cons 1 EmptySet) []]
+
+
+deleteTree x ts = mapTree (\ s -> delete x s) ts
