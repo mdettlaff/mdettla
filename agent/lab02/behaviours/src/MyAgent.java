f07392f6540a440eab5aaea252c30c49dc4b7007
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2011-02-15 19:18:02 +0000

    agent: build.xml dla JADE

--- agent/lab02/behaviours/src/MyAgent.java
+++ agent/lab02/behaviours/src/MyAgent.java
@@ -1,34 +1,38 @@
 import jade.core.Agent;
 import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.OneShotBehaviour;
 import jade.core.behaviours.TickerBehaviour;
 import jade.core.behaviours.WakerBehaviour;
 
 public class MyAgent extends Agent {
-	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void setup() {
-		System.out.println("Moj agent: setup");
+		System.out.println("Mój agent: setup");
+
+		Behaviour oneShot = new OneShotBehaviour(this) {
+			@Override
+			public void action() {
+				System.out.println("Nazywam się " + getAID().getName());
+			}
+		};
+		addBehaviour(oneShot);
 
 		Behaviour waker = new WakerBehaviour(this, 4000) {
-			private static final long serialVersionUID = 1L;
-
 			@Override
 			protected void onWake() {
 				System.out.println("Cthulhu budzi sie ze snu!!!");
 			}
 		};
+		addBehaviour(waker);
 
 		Behaviour ticker = new TickerBehaviour(this, 5000) {
 			private static final long serialVersionUID = 1L;
-
 			@Override
 			protected void onTick() {
-				System.out.println("Cthulhu zjadl kultyste.");
+				System.out.println("Cthulhu zjadł kultystę.");
 			}
 		};
-
-		addBehaviour(waker);
 		addBehaviour(ticker);
 	}
 
