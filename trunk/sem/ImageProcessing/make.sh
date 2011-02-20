928ee51d23151ab672b1720637433b9352ee039e
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2011-02-20 14:23:07 +0000

    Dodanie algorytmu watershed, uproszczenie skryptu.

--- trunk/sem/ImageProcessing/make.sh
+++ trunk/sem/ImageProcessing/make.sh
@@ -8,27 +8,25 @@ then
   src/mdettla/imgproc/algorithms/fuzzy/edge/*.java \
   src/mdettla/imgproc/algorithms/fuzzy/noiseremoval/*.java \
   src/mdettla/imgproc/algorithms/edge/*.java
+elif [ $1 == "edge" ]
+then
+  java -cp bin mdettla.imgproc.ProcessImage \
+  mdettla.imgproc.algorithms.edge.SobelAlgorithm $2 $3
+elif [ $1 == "fuzzyedge" ]
+then
+  java -cp bin mdettla.imgproc.ProcessImage \
+  mdettla.imgproc.algorithms.fuzzy.edge.EdgeDetectionWithFuzzyClassifier \
+  $2 $3
+elif [ $1 == "mff" ]
+then
+  java -cp bin mdettla.imgproc.ProcessImage \
+  mdettla.imgproc.algorithms.fuzzy.noiseremoval.MultipassFuzzyFilter \
+  $2 $3
+elif [ $1 == "ws" ]
+then
+  java -cp bin mdettla.imgproc.ProcessImage \
+  mdettla.imgproc.algorithms.segmentation.WatershedSegmentation \
+  $2 $3
 else
-  if [ $1 == "edge" ]
-  then
-    java -cp bin mdettla.imgproc.ProcessImage \
-    mdettla.imgproc.algorithms.edge.SobelAlgorithm $2 $3
-  else
-    if [ $1 == "fuzzyedge" ]
-    then
-      java -cp bin mdettla.imgproc.ProcessImage \
-      mdettla.imgproc.algorithms.fuzzy.edge.EdgeDetectionWithFuzzyClassifier \
-      $2 $3
-    else
-      if [ $1 == "mff" ]
-      then
-        java -cp bin mdettla.imgproc.ProcessImage \
-        mdettla.imgproc.algorithms.fuzzy.noiseremoval.MultipassFuzzyFilter \
-        $2 $3
-      else
-        echo "Nieznany parametr"
-      fi
-    fi
-  fi
+  echo "Nieznany parametr"
 fi
-
