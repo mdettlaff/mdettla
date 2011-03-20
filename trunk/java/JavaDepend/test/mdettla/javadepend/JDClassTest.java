0b98726634df8f737d95e1df5bc57ee5184dc46e
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2011-03-20 22:28:38 +0000

    java: JavaDepend, początkowa wersja

---
+++ trunk/java/JavaDepend/test/mdettla/javadepend/JDClassTest.java
@@ -0,0 +1,18 @@
+package mdettla.javadepend;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class JDClassTest {
+
+	@Test
+	public void fromJavaSourceCode() {
+		final String sourceCode =
+			"package foo.bar\n" +
+			"public class A {\n" +
+			"}";
+		JDClass A = JDClass.fromJavaSourceCode(sourceCode);
+		assertEquals("foo.bar.A", A.getFullName());
+	}
+}
