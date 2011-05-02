a008eaacfe4adf0c4b3f3f7dcbd8fdea9f4e4776
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2011-05-02 11:59:11 +0000

    keyboard: dodanie pliku build.xml

--- trunk/keyboard/test/mdettla/jga/core/GeneticAlgorithmIntegrationTest.java
+++ trunk/keyboard/test/mdettla/jga/core/GeneticAlgorithmIntegrationTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
-public class GeneticAlgorithmTest {
+public class GeneticAlgorithmIntegrationTest {
 
 	private static final int ITERATIONS = 100;
 
