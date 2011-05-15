b61dd556393135c24d2aef0a5ae6d8c03197a155
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2011-05-15 19:59:13 +0000

    javaee: CR: wydzielenie klasy FileDownloader, dodanie drugiego raportu

--- trunk/javaee/CR/src/main/java/parasoft/centrap/web/ReportController.java
+++ trunk/javaee/CR/src/main/java/parasoft/centrap/web/ReportController.java
@@ -1,17 +1,14 @@
 package parasoft.centrap.web;
 
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.io.Serializable;
 
 import javax.enterprise.context.SessionScoped;
-import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 import javax.inject.Named;
-import javax.servlet.http.HttpServletResponse;
 
-import parasoft.centrap.reports.RaportZakonczonychPromocji;
 import parasoft.centrap.reports.Report;
 import parasoft.centrap.reports.ReportFilter;
+import parasoft.centrap.web.util.FileDownloader;
 
 @Named
 @SessionScoped
@@ -19,20 +16,27 @@ public class ReportController implements Serializable {
 
 	private ReportFilter reportFilter;
 	private Report report;
+	private FileDownloader fileDownloader;
 
 	public ReportController() {
 		this.reportFilter = new ReportFilter();
-		report = new RaportZakonczonychPromocji();
+	}
+
+	@Inject
+	public ReportController(FileDownloader fileDownloader) {
+		this();
+		this.fileDownloader = fileDownloader;
 	}
 
 	public ReportFilter getReportFilter() {
 		return reportFilter;
 	}
 
-	public String startReport() {
+	public String startReport(Report report) {
+		this.report = report;
 		report.setFilter(reportFilter);
 		report.start();
-		return "success";
+		return "report-success";
 	}
 
 	public String getHtmlResults() {
@@ -40,35 +44,12 @@ public class ReportController implements Serializable {
 	}
 
 	public void downloadPlainTextReport() {
-		downloadFile(report.getPlainTextResults(), "report.txt", "text/plain");
+		fileDownloader.downloadFile(
+				report.getPlainTextResults(), "report.txt", "text/plain");
 	}
 
 	public void downloadCsvReport() {
-		downloadFile(report.getCsvResults(), "report.csv", "text/csv");
-	}
-
-	private void downloadFile(
-			String content, String filename, String mimeType) {
-		try {
-			FacesContext context = FacesContext.getCurrentInstance();
-			HttpServletResponse response =
-				getFileDownloadResponse(context, filename, mimeType);
-			PrintWriter writer = response.getWriter();
-			writer.println(content);
-			context.responseComplete();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	private HttpServletResponse getFileDownloadResponse(
-			FacesContext context, String filename, String mimeType) {
-		HttpServletResponse response =
-			(HttpServletResponse)context.getExternalContext().getResponse();
-		response.setHeader(
-				"Content-disposition",
-				"attachment; filename=" + filename);
-		response.setContentType(mimeType + "; charset=UTF-8");
-		return response;
+		fileDownloader.downloadFile(
+				report.getCsvResults(), "report.csv", "text/csv");
 	}
 }
