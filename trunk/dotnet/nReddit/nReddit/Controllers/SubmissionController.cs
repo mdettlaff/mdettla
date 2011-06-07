9b76fe98cc4ca8f0c678d650b08b6b499d55a45a
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2011-06-07 17:32:29 +0000

    dotnet: nReddit: struktura z subredditami i komentarzami

---
+++ trunk/dotnet/nReddit/nReddit/Controllers/SubmissionController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using nReddit.Models;
+
+namespace nReddit.Controllers
+{ 
+    public class SubmissionController : Controller
+    {
+        private NRedditEntities db = new NRedditEntities();
+
+        //
+        // GET: /Submission/
+
+        public ViewResult Index()
+        {
+            return View(db.Submissions.ToList());
+        }
+
+        //
+        // GET: /Submission/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Submission submission = db.Submissions.Find(id);
+            return View(submission);
+        }
+
+        //
+        // GET: /Submission/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Submission/Create
+
+        [HttpPost]
+        public ActionResult Create(Submission submission)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Submissions.Add(submission);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            return View(submission);
+        }
+        
+        //
+        // GET: /Submission/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            Submission submission = db.Submissions.Find(id);
+            return View(submission);
+        }
+
+        //
+        // POST: /Submission/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Submission submission)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(submission).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(submission);
+        }
+
+        //
+        // GET: /Submission/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            Submission submission = db.Submissions.Find(id);
+            return View(submission);
+        }
+
+        //
+        // POST: /Submission/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            Submission submission = db.Submissions.Find(id);
+            db.Submissions.Remove(submission);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
