6b4e4271246a249e5e3e171266fca871eb656feb
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2011-06-08 09:32:25 +0000

    dotnet: nReddit: adnotacje w Modelu, poprawki w widokach (sortowanie)

--- dotnet/nReddit/nReddit/Models/Submission.cs
+++ dotnet/nReddit/nReddit/Models/Submission.cs
@@ -2,27 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace nReddit.Models
 {
     public class Submission
     {
         public int SubmissionID { get; set; }
+        [Required(ErrorMessage = "Musisz podać URL")]
+        [RegularExpression(@"^http://.+\..+",
+            ErrorMessage = "Podany URL jest nieprawidłowy")]
         public string Url { get; set; }
+        [Required(ErrorMessage = "Musisz podać tytuł")]
+        [DisplayName("Tytuł")]
+        [StringLength(100)]
         public string Title { get; set; }
+        [DisplayName("Opis")]
         public string Text { get; set; }
+        [ScaffoldColumn(false)]
         public int UpvoteCount { get; set; }
+        [ScaffoldColumn(false)]
         public int DownvoteCount { get; set; }
         public ICollection<Comment> Comments { get; set; }
         public int SubredditID { get; set; }
         public Subreddit Subreddit { get; set; }
 
+        [ScaffoldColumn(false)]
         public int Score
         {
             get
             {
                 return UpvoteCount - DownvoteCount;
             }
+        }
+
+        public void Upvote()
+        {
+            UpvoteCount++;
+        }
+
+        public void Downvote()
+        {
+            DownvoteCount++;
         }
     }
 }
