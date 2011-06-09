d55201864c7d8daf7bd1bfe2507c55897c8a76f9
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2011-06-09 08:22:04 +0000

    dotnet: nReddit: dodanie kanału RSS

--- trunk/dotnet/nReddit/nReddit/Controllers/HomeController.cs
+++ trunk/dotnet/nReddit/nReddit/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using nReddit.Models;
+using nReddit.Util;
 
 namespace nReddit.Controllers
 {
@@ -18,6 +19,27 @@ namespace nReddit.Controllers
         {
             ICollection<Submission> submissions = db.Submissions.ToList();
             return View(submissions);
+        }
+
+        public ActionResult Rss()
+        {
+            ICollection<Submission> submissions = db.Submissions.ToList();
+            string feed = "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
+            feed += "<rss version=\"2.0\">";
+            feed += "<channel>";
+            feed += "<title>nReddit</title>";
+            foreach (Submission submission in submissions) {
+                feed += "<item>";
+                feed += "<title>";
+                feed += submission.Title;
+                feed += "</title>";
+                feed += "<link>" + submission.Url + "</link>";
+                feed += "<description>" + submission.Text + "</description>";
+                feed += "</item>";
+            }
+            feed += "</channel>";
+            feed += "</rss>";
+            return new RssActionResult(feed);
         }
     }
 }
---
+++ trunk/dotnet/nReddit/nReddit/Util/RssActionResult.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace nReddit.Util
+{
+    public class RssActionResult : ActionResult
+    {
+        private string content = "Empty";
+
+        public RssActionResult(string content)
+        {
+            this.content = content;
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            context.HttpContext.Response.ContentType = "text/xml";
+
+            TextWriter writer = context.HttpContext.Response.Output;
+            writer.Write(content);
+            writer.Close();
+        }
+    }
+}
