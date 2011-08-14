493bd610ca284408326ca671b29e590d123dedb1
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2011-08-14 14:00:54 +0000

    thesis: zmiana nazwy xml.bib -> bib.bib

--- trunk/thesis/make.sh
+++ trunk/thesis/make.sh
@@ -2,7 +2,7 @@ SIGMA_DIR=seminarium/thesis
 
 if [ "$1" == "" ]
 then
-  scp magisterka.tex xml.bib sigma:$SIGMA_DIR
+  scp magisterka.tex *.bib sigma:$SIGMA_DIR
   scp fig/*.* sigma:$SIGMA_DIR/fig
   ssh sigma "cd $SIGMA_DIR && make"
   scp sigma:$SIGMA_DIR/magisterka.pdf /tmp
