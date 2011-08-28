4b954905fec16821a73a33d76676df8e4513b725
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2011-08-28 14:13:41 +0000

    keyboard: zmiana SwapMutation i dodanie MultipleSwapMutation

--- trunk/keyboard/run.sh
+++ trunk/keyboard/run.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
+set -e -u
+
 GA="java -cp build/classes mdettla.keyboard.ga.GAKeyboard quiet=true"
 RESULTS_FILE="results/results.txt"
 EN_CORPUS="src/mdettla/keyboard/ga/resources/en/*.txt"
 
-echo -n > $RESULTS_FILE
-$GA populationSize=20 generationsCount=50 $EN_CORPUS >> $RESULTS_FILE
-echo -e "\n" >> $RESULTS_FILE
-$GA populationSize=30 generationsCount=60 $EN_CORPUS >> $RESULTS_FILE
+function epochs {
+  for i in `seq 4`
+  do
+    echo $@
+    $GA $@ $EN_CORPUS | grep 'przystosowanie:'
+  done
+  echo
+}
+
+epochs mutationOperator=mdettla.jga.operators.mutation.SwapMutation
+epochs mutationOperator=mdettla.jga.operators.mutation.MultipleSwapMutation
+epochs mutationOperator=mdettla.jga.operators.mutation.DisplacementMutation
+epochs mutationOperator=mdettla.jga.operators.mutation.InsertionMutation
+epochs mutationOperator=mdettla.jga.operators.mutation.InversionMutation
+epochs mutationOperator=mdettla.jga.operators.mutation.ScrambleMutation
+epochs mutationOperator=mdettla.jga.operators.mutation.SimpleInversionMutation
