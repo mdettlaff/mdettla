78968240c207348756951498ee43b1c713995f36
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2011-08-30 16:56:59 +0000

    thesis: nowe wyniki dla języka polskiego

--- trunk/thesis/scripts/KeyLayoutGen/run.sh
+++ trunk/thesis/scripts/KeyLayoutGen/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+
 set -u
 
-java -cp bin scripts.KeyLayoutGen /tmp/$1.png
-convert /tmp/$1.png ../../fig/$1.jpg
+java -cp $DIR/bin scripts.KeyLayoutGen /tmp/$1.png
+convert /tmp/$1.png $DIR/../../fig/$1.jpg
 
