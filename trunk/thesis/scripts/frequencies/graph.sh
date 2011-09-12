2147600d761cff895cb66f827e6746ee78679ffa
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2011-09-12 19:17:44 +0000

    thesis: uaktualnienie wykresu frequencies

--- trunk/thesis/scripts/frequencies/graph.sh
+++ trunk/thesis/scripts/frequencies/graph.sh
@@ -4,10 +4,17 @@ set -u
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-$DIR/frequencies.py \
-  /home/mdettla/workspace/Keyboard/src/mdettla/keyboard/ga/resources/en/otoos11.txt \
-  /home/mdettla/workspace/Keyboard/src/mdettla/keyboard/ga/resources/pl/pg8119.txt \
-  > /tmp/data_frequencies.dat
+echo > /tmp/english.txt
+for file in $HOME/workspace/Keyboard/src/mdettla/keyboard/ga/resources/en/*.txt
+do
+  cat $file >> /tmp/english.txt
+done
+echo > /tmp/polish.txt
+for file in $HOME/workspace/Keyboard/src/mdettla/keyboard/ga/resources/pl/*.txt
+do
+  cat $file >> /tmp/polish.txt
+done
+$DIR/frequencies.py /tmp/english.txt /tmp/polish.txt > /tmp/data_frequencies.dat
 cat /tmp/data_frequencies.dat \
   | grep "[a-z] [0-9]\+\.[0-9]\+ [0-9]\+\.[0-9]\+" > /tmp/data.dat
 
@@ -16,12 +23,11 @@ echo 'set terminal png size 640, 320 crop' > $gp_file
 echo 'set output "/tmp/chars_freqs.png"' >> $gp_file
 echo 'set grid' >> $gp_file
 echo 'unset label' >> $gp_file
-echo 'set xlabel "Znak"' >> $gp_file
 echo 'set ylabel "Czestotliwosc wystepowania (%)"' >> $gp_file
 echo 'set style data histograms' >> $gp_file
 echo 'set style fill solid 1.0 border -1' >> $gp_file
-echo 'plot "/tmp/data.dat" using 2:xtic(1),\\' >> $gp_file
-echo '     "/tmp/data.dat" using 3:xtic(1)' >> $gp_file
+echo 'plot "/tmp/data.dat" using 2:xtic(1) ti "j. polski",\\' >> $gp_file
+echo '     "/tmp/data.dat" using 3:xtic(1) ti "j. angielski"' >> $gp_file
 
 gnuplot $gp_file
 convert /tmp/chars_freqs.png $DIR/../../fig/frequencies.jpg
