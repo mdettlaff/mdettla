da1791774d79223e41e6a59a2186cfac9a7995c1
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2011-11-06 09:08:37 +0000

    python: skrypty do importów Javy

--- trunk/python/java_imports/import_static.sh
+++ trunk/python/java_imports/import_static.sh
@@ -10,14 +10,24 @@ function add_import {
   sed -i "s/^\(package .*\)/\1\n\nimport $class;/" $file
 }
 
+if [ $# -lt 2 ]
+then
+  echo "Usage: $0 STATIC_METHOD JAVA_FILES..."
+  exit 2
+fi
+
 class_and_method=`echo $1 | sed "s/.*\.\(.*\..*\)/\1/"`
 method=`echo $class_and_method | sed "s/.*\.\(.*\)/\1/"`
 non_static_import=`echo $1 | sed "s/\(.*\)\..*/\1/"`
 
-file=$2
+files=( $@ )
+unset files[0]
 
-sed -i "s/import $non_static_import;//" $file
-sed -i "s/$class_and_method/$method/" $file
-add_import "static $1" $file
-$SCRIPT_DIR/organize_imports.py $file
+for file in ${files[@]}
+do
+  sed -i "s/import $non_static_import;//" $file
+  sed -i "s/$class_and_method/$method/" $file
+  add_import "static $1" $file
+  $SCRIPT_DIR/organize_imports.py $file
+done
 
