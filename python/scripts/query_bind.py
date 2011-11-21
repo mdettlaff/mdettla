439253a238df679992775a5aed18a086b27b6d13
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2011-11-21 13:15:21 +0000

    python: poprawienie skryptu query_bind.py

--- python/scripts/query_bind.py
+++ python/scripts/query_bind.py
@@ -19,11 +19,11 @@ def bind_parameters(query, query_parameters):
 
 def get_parameters(bind_line):
     """
-    >>> get_parameters('        bind => [2011-11-28, 1970-01-01 00:00:00.0, true, 2, 3.50, BASE]')
-    ["'2011-11-28'", "'1970-01-01 00:00:00.0'", "'true'", '2', '3.50', "'BASE'"]
+    >>> get_parameters('        bind => [2011-11-28, 1970-01-01 00:00:00.0, true, 2, 3.50, BASE, null]')
+    ["'2011-11-28'", "'1970-01-01 00:00:00.0'", "'true'", '2', '3.50', "'BASE'", 'null']
     """
     is_number = lambda s: re.match(r'^\d+(\.\d+)?$', s)
-    parse_param = lambda p: p if is_number(p) else "'" + p + "'"
+    parse_param = lambda p: p if is_number(p) or p == 'null' else "'" + p + "'"
     return [parse_param(p) for p in re.sub(BIND_REGEX, '\\1', bind_line).split(', ')]
 
 def parse_input(input_lines):
