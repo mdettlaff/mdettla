4ebc1793e771702d01b577252cfdc2cdb8564fcf
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2011-12-25 16:53:01 +0000

    python: dodanie pyfold.py

---
+++ trunk/python/scripts/pyfold.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- encoding: UTF-8 -*-
+
+"""Replacement for "fold -w" that can handle UTF-8 correctly."""
+
+import doctest
+import re
+import sys
+
+
+def fold(text, width):
+    """
+    >>> fold('', 80)
+    ''
+    >>> fold(' ', 80)
+    ' '
+    >>> fold('foo', 80)
+    'foo'
+    >>> fold(' foo ', 80)
+    ' foo '
+    >>> fold('foo bar', 4)
+    'foo\\nbar'
+    >>> fold('foo  bar', 4)
+    'foo\\n  bar'
+    >>> fold('fo  b', 4)
+    'fo\\n  b'
+    >>> fold('foo bar', 5)
+    'foo\\nbar'
+    >>> fold('foo bar', 6)
+    'foo\\nbar'
+    >>> fold('foo bar', 7)
+    'foo bar'
+    >>> fold('foo bar', 80)
+    'foo bar'
+    >>> fold('foo bar baz', 10)
+    'foo bar\\nbaz'
+    >>> fold('foo bar baz qux quux', 10)
+    'foo bar\\nbaz qux\\nquux'
+    >>> fold('foobar', 3)
+    'foo\\nbar'
+    >>> fold('foobarbazquux', 3)
+    'foo\\nbar\\nbaz\\nquu\\nx'
+    >>> fold('foobar b az', 4)
+    'foob\\nar b\\naz'
+    >>> fold('foo\\n', 80)
+    'foo\\n'
+    >>> fold('foo\\n\\n\\nbar b qux', 5)
+    'foo\\n\\n\\nbar b\\nqux'
+    """
+    LINEWHITESPACE = '[ \t]+'
+    islinewhitespace = lambda x: re.match(s, LINEWHITESPACE)
+    line_len = 0
+    tokens = re.split('(' + LINEWHITESPACE + '|\n)', text)
+    i = 0
+    while i < len(tokens):
+        if '\n' == tokens[i]:
+            line_len = 0
+        else:
+            line_len += len(tokens[i])
+        if ' ' in tokens[i] and line_len + len(tokens[i + 1]) > width:
+            tokens[i] = '\n' if tokens[i] == ' ' else '\n' + tokens[i]
+            line_len = 0
+        elif not tokens[i].isspace() and len(tokens[i]) > width:
+            tokens.insert(i, tokens[i][:width] + '\n')
+            tokens[i + 1] = tokens[i + 1][width:]
+            line_len = 0
+        i += 1
+    return ''.join(tokens)
+
+
+doctest.testmod()
+
+if __name__ == '__main__':
+    width = int(sys.argv[1]) if len(sys.argv) > 1 else 80
+    #input_lines = ''.join(sys.stdin.readlines())
+    #print fold(input_lines, width),
+
