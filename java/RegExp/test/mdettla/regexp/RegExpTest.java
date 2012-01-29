df96b8f3b822ce9b75c68688dddc41cc9ac3a29b
wundzunhome@wp.pl <wundzunhome@wp.pl@1d19430a-0295-11de-9d1a-b59b2e1864b6>
2012-01-29 22:36:23 +0000

    java: RegExp: dodatkowe testy

--- java/RegExp/test/mdettla/regexp/RegExpTest.java
+++ java/RegExp/test/mdettla/regexp/RegExpTest.java
@@ -9,7 +9,21 @@ public class RegExpTest {
 
 	@Test
 	public void test() {
+		assertTrue(new RegExp("a").match("a"));
+		assertTrue(new RegExp("ab").match("ab"));
 		assertTrue(new RegExp("a").match("aa"));
+		assertTrue(new RegExp("ab").match("aba"));
+		assertTrue(new RegExp("a|b|c").match("c"));
+		assertTrue(new RegExp("(ab|cd|ef)+").match("cd"));
+		assertTrue(new RegExp("(ab|cd|ef)+").match("abcdabef"));
+		assertTrue(new RegExp("a*").match("aaaaa"));
+		assertTrue(new RegExp("ba*c").match("baaac"));
+		assertTrue(new RegExp("ba*c").match("bc"));
+		assertTrue(new RegExp("ba*c").match("bac"));
 		assertFalse(new RegExp("a").match("b"));
+		assertFalse(new RegExp("ba").match("aba"));
+		assertFalse(new RegExp("a").match(""));
+		assertFalse(new RegExp("(ab|cd|ef)+").match("a"));
+		assertFalse(new RegExp("ba*c").match("baaaa"));
 	}
 }
