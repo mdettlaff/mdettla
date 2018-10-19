244d2528db4911c2fbc5bfbad20708ffb5e65c2c
Michał Dettlaff <mdettlaff@jitsolutions.pl>
2018-10-19 20:10:45 +0200

    kanban forecasting script - hours

--- python/kanban_forecast.py
+++ python/kanban_forecast.py
@@ -12,19 +12,20 @@ DEADLINE_IN_DAYS = int(sys.argv[1])
 DEVELOPERS_COUNT = float(sys.argv[2])
 STORY_COUNT = int(sys.argv[3])
 
-# lead times for all recent done stories, in days
+# lead times for all recent done stories, in hours
 LEAD_TIMES_INPUT = """
-1 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 5 5 5 5 5 5 5
-5 5 5 5 5 5 5 5 5 5 5 8 8 8 8 8 8 8 13 13
+SPRINT 20:
+5 5 8 8 13 13 20 20 30 30
+SPRINT 21:
+5 8 8 13 13 20 20 30 30 30 50
+5 8 8 13 13 20 20 30 30 30 50
 """
-LEAD_TIMES = map(lambda x: float(x), filter(lambda x: x.isdigit(),
-    LEAD_TIMES_INPUT.strip().replace('\n', ' ').split(' ')))
 SIMULATION_COUNT = 10000
 
-# magic multiplier
-LEAD_TIMES = [x * 0.75 * 0.75 for x in LEAD_TIMES]
+lead_times = map(lambda x: float(x), filter(lambda x: x.isdigit(),
+    LEAD_TIMES_INPUT.strip().replace('\n', ' ').split(' ')))
 
-#print('lead times: ' + str(LEAD_TIMES))
+#print('lead times: ' + str(lead_times))
 
 # simulated durations in days
 simulations = []
@@ -32,11 +33,11 @@ success_count = 0.0
 for i in xrange(SIMULATION_COUNT):
     linear_duration = 0
     for j in xrange(STORY_COUNT):
-        ub_story_time = random.choice(LEAD_TIMES)
+        ub_story_time = random.choice(lead_times)
         linear_duration += ub_story_time
     duration = linear_duration / DEVELOPERS_COUNT
     simulations.append(duration)
-    if duration <= DEADLINE_IN_DAYS:
+    if duration <= DEADLINE_IN_DAYS * 8:
         success_count += 1
 success_probability = success_count / SIMULATION_COUNT
 
