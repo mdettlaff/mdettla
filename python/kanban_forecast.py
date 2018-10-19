a6d0daca2d0686e4aadeeff03e284a8f15ed4440
Michał Dettlaff <mdettlaff@jitsolutions.pl>
2018-10-19 20:25:02 +0200

    kanban forecasting script - read lead times from stdin

--- python/kanban_forecast.py
+++ python/kanban_forecast.py
@@ -4,27 +4,21 @@
 import random
 import sys
 
-if (len(sys.argv) != 4):
-    print('usage: ' + sys.argv[0] + ' DEADLINE_IN_DAYS DEVELOPERS_COUNT STORY_COUNT')
-    sys.exit(2)
-
-DEADLINE_IN_DAYS = int(sys.argv[1])
-DEVELOPERS_COUNT = float(sys.argv[2])
-STORY_COUNT = int(sys.argv[3])
-
-# lead times for all recent done stories, in hours
-LEAD_TIMES_INPUT = """
-SPRINT 20:
-5 5 8 8 13 13 20 20 30 30
-SPRINT 21:
-5 8 8 13 13 20 20 30 30 30 50
-5 8 8 13 13 20 20 30 30 30 50
-"""
 SIMULATION_COUNT = 10000
 
-lead_times = map(lambda x: float(x), filter(lambda x: x.isdigit(),
-    LEAD_TIMES_INPUT.strip().replace('\n', ' ').split(' ')))
+if (len(sys.argv) != 4):
+    print('usage: ' + sys.argv[0] + ' DEADLINE_IN_DAYS DEVELOPERS_COUNT STORY_COUNT < LEAD_TIMES')
+    sys.exit(2)
 
+deadline_in_days = int(sys.argv[1])
+developers_count = float(sys.argv[2])
+story_count = int(sys.argv[3])
+
+# lead times for all recent done stories, in hours
+lead_times_input = ''.join(sys.stdin.readlines())
+
+lead_times = map(lambda x: float(x), filter(lambda x: x.isdigit(),
+    lead_times_input.strip().replace('\n', ' ').split(' ')))
 #print('lead times: ' + str(lead_times))
 
 # simulated durations in days
@@ -32,12 +26,12 @@ simulations = []
 success_count = 0.0
 for i in xrange(SIMULATION_COUNT):
     linear_duration = 0
-    for j in xrange(STORY_COUNT):
+    for j in xrange(story_count):
         ub_story_time = random.choice(lead_times)
         linear_duration += ub_story_time
-    duration = linear_duration / DEVELOPERS_COUNT
+    duration = linear_duration / developers_count
     simulations.append(duration)
-    if duration <= DEADLINE_IN_DAYS * 8:
+    if duration <= deadline_in_days * 8:
         success_count += 1
 success_probability = success_count / SIMULATION_COUNT
 
